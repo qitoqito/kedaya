5dcb61767a4dd3c43aabe0a4b8a1828d7784f616
air <>
2021-11-07 17:31:26 +0800

    package

--- config/test.js
+++ config/test.js
@@ -1,3 +1,8 @@
 module.exports = {
-    'help': 234
+    'help': 234,
+    "BARK_TOKEN": "xxxxxx",
+    "BARK_URL": "http://bark.xxxx.com",
+    "BARK_SOUND": "shake",
+    "TG_TOKEN": "242423423:aaaaaaaa",
+    "TG_ID": "2342423423",
 }
