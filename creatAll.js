f3eb09f1495ed4397a607d6a8477eb57bfd9c8b7
可达鸭 <92066902+qitoqito@users.noreply.github.com>
2021-12-04 16:34:05 +0800

    京喜购物返现

---
+++ creatAll.js
@@ -0,0 +1,41 @@
+let fs = require("fs")
+let dirname = process.mainModule.path
+let dir = fs.readdirSync(`${dirname}/parse`);
+let dicts = {}
+let prefix = process.env.CREAT_PREFIX || ''
+dir.forEach(function(item, index) {
+    let stat = fs.lstatSync(`${dirname}/parse/` + item)
+    if (stat.isDirectory() === true) {
+        dicts[item] = fs.readdirSync(`${dirname}/parse/${item}`)
+    }
+})
+let pathFile = fs.readdirSync(dirname)
+let content = `
+!(async () => {
+        let prefix = process.env.CREAT_PREFIX ||''
+        let filename = process.mainModule.filename.replace(prefix,'').match(/(\\w+).js/)[1]
+        let dirname = process.mainModule.path
+        let type = filename.split('_')[0]
+        if (['js', 'jx', 'jr', 'jw'].includes(type)) {
+            type = 'jd'
+        }
+        let main = require(\`\${dirname}/parse/\${type}/\${filename}\`)
+        let a = new main()
+        await a.init({})
+    }
+)().catch((e) => {
+    console.log(e.message)
+})
+
+`
+for (let i in dicts) {
+    for (let j of dicts[i]) {
+        let filename = `${prefix}${j}`
+        fs.writeFile(`${dirname}/${filename}`, content, function(err, data) {
+            if (err) {
+                throw err;
+            }
+            console.log(`${filename}写入成功`)
+        })
+    }
+}
---
+++ parse/jd/jx_help_cashback.js
@@ -0,0 +1,53 @@
+const Template = require('../../template');
+
+class Main extends Template {
+    constructor() {
+        super()
+        this.title = "京喜购物返红包助力"
+        this.cron = "15 */2 * * *"
+        this.task = 'all'
+        this.help = 6
+        this.import = ['jdAlgo']
+        this.verify = 1
+        this.readme=`如需修改查询返现人数,请自行添加环境变量\nexport ${this.filename}_help=人数
+        `
+    }
+
+    async prepare() {
+        this.algo = new this.modules.jdAlgo()
+        let url = `https://wq.jd.com/bases/orderlist/list?order_type=3&start_page=1&last_page=0&page_size=10&callersource=newbiz&t=${this.timestamp}&traceid=&g_ty=ls&g_tk=606717070`
+        for (let j of this.cookies['help']) {
+            this.setCookie(j);
+            await this.curl(url)
+            try {
+                for (let k of this.source.orderList) {
+                    try {
+                        let orderid = k.parentId != '0' ? k.parentId : k.orderId
+                        let url = `https://wq.jd.com/fanxianzl/zhuli/QueryGroupDetail?isquerydraw=1&orderid=${orderid}&groupid=&_=1619853863315&sceneval=2&g_login_type=1&g_ty=ls`
+                        let dec = await this.algo.dec(url)
+                        await this.curl(dec.url)
+                        let now = parseInt(new Date() / 1000)
+                        let end = this.source.data.groupinfo.end_time
+                        if (end>now && this.source.data.groupinfo.openhongbaosum != this.source.data.groupinfo.totalhongbaosum) {
+                            let groupid = this.source.data.groupinfo.groupid;
+                            this.shareCode.push({
+                                'groupid': groupid
+                            })
+                        }
+                    } catch (e) {
+                        console.log(e.message());
+                    }
+                }
+            } catch (e) {
+            }
+        }
+    }
+
+    async main(id) {
+        id.url = `http://wq.jd.com/fanxianzl/zhuli/Help?groupid=${id.inviter.groupid}&_stk=groupid&_ste=2&g_ty=ls&g_tk=1710198667&sceneval=2&g_login_type=1`
+        let s = await this.algo.curl(id)
+        console.log(this.haskey(s, 'data.prize.discount'))
+    }
+}
+
+module.exports = Main;
