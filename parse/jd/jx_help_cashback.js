858fe99ad176212a2957ed7bcbd6a28ebbe2888a
可达鸭 <92066902+qitoqito@users.noreply.github.com>
2021-12-05 15:28:48 +0800

    pingou cashback

--- parse/jd/jx_help_cashback.js
+++ parse/jd/jx_help_cashback.js
@@ -4,40 +4,51 @@ class Main extends Template {
     constructor() {
         super()
         this.title = "京喜购物返红包助力"
-        this.cron = "15 7-23/3 * * *"
+        this.cron = "15 */2 * * *"
         this.task = 'all'
-        this.help = 6
+        this.help = 'main'
         this.import = ['jdAlgo']
         this.verify = 1
-        this.readme=`如需修改查询返现人数,请自行添加环境变量\nexport ${this.filename}_help=人数
-        `
     }
 
     async prepare() {
         this.algo = new this.modules.jdAlgo()
         this.algo.set({
-            'appId': 10028,
+            'appId': 10022,
             type: 'pingou',
             // verify: 1
         })
         let url = `https://wq.jd.com/bases/orderlist/list?order_type=3&start_page=1&last_page=0&page_size=10&callersource=newbiz&t=${this.timestamp}&traceid=&g_ty=ls&g_tk=606717070`
-        for (let j of this.cookies['help']) {
-            this.setCookie(j);
-            await this.curl(url)
+        for (let cookie of this.cookies['help']) {
+            let s = await this.curl({
+                    url,
+                    cookie
+                }
+            )
             try {
-                for (let k of this.source.orderList) {
+                for (let k of s.orderList) {
                     try {
                         let orderid = k.parentId != '0' ? k.parentId : k.orderId
+                        console.log(`正在查询订单: ${orderid}`)
                         let url = `https://wq.jd.com/fanxianzl/zhuli/QueryGroupDetail?isquerydraw=1&orderid=${orderid}&groupid=&_=1619853863315&sceneval=2&g_login_type=1&g_ty=ls`
-                        let dec = await this.algo.dec(url)
-                        await this.curl(dec.url)
-                        let now = parseInt(new Date() / 1000)
-                        let end = this.source.data.groupinfo.end_time
-                        if (end>now && this.source.data.groupinfo.openhongbaosum != this.source.data.groupinfo.totalhongbaosum) {
-                            let groupid = this.source.data.groupinfo.groupid;
-                            this.shareCode.push({
-                                'groupid': groupid
-                            })
+                        let ss = await this.algo.curl({
+                                url,
+                                cookie
+                            }
+                        )
+                        if (ss.data.groupinfo) {
+                            let now = parseInt(new Date() / 1000)
+                            let end = ss.data.groupinfo.end_time
+                            if (end>now && ss.data.groupinfo.openhongbaosum != ss.data.groupinfo.totalhongbaosum) {
+                                let groupid = ss.data.groupinfo.groupid;
+                                this.shareCode.push({
+                                    'groupid': groupid
+                                })
+                                await this.curl({
+                                    'url': `http://m-jd.cn/tool/jingxi/?key=${encodeURIComponent(`https://wqsd.jd.com/sns/201907/25/rebate/index.html?groupid=${groupid}`)}`,
+                                    cookie: ''
+                                })
+                            }
                         }
                     } catch (e) {
                         console.log(e.message());
