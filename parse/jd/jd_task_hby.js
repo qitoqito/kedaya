71151f53822a2bc43fa52508c7cb2336b781015e
可达鸭 <92066902+qitoqito@users.noreply.github.com>
2021-12-27 20:13:42 +0800

    Update jd_task_hby.js

--- parse/jd/jd_task_hby.js
+++ parse/jd/jd_task_hby.js
@@ -3,17 +3,17 @@ const Template = require('../../template');
 class Main extends Template {
     constructor() {
         super()
-        this.title = "京东1212红包雨"
-        // this.cron = "1 19-23 * * *"
-        this.task = 'all'
-        this.thread = 7
+        this.title = "京东元旦红包雨"
+        this.cron = "1 19-23 * * *"
+        this.task = 'local'
+        this.thread = 6
     }
 
     async main(p) {
         let cookie = p.cookie
         let s = await this.curl({
                 'url': `https://api.m.jd.com/client.action`,
-                'form': `functionId=hby_lottery&appid=publicUseApi&body={"babelProjectId":"01102969","babelPageId":"3222650"}&t=${this.timestamp}&client=wh5&clientVersion=1.0.0&networkType=&ext={"prstate":"0"}`,
+                'form': `functionId=hby_lottery&appid=publicUseApi&body={"babelProjectId":"01128912","babelPageId":"3195530"}&t=${this.timestamp}&client=wh5&clientVersion=1.0.0&networkType=&ext={"prstate":"0"}`,
                 cookie
             }
         )
