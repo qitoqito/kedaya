af569815c1b5e5627ad0a5c10114f623763e5319
可达鸭 <92066902+qitoqito@users.noreply.github.com>
2022-01-10 21:26:49 +0800

    Create jd_task_supBox.js

---
+++ parse/jd/jd_task_supBox.js
@@ -0,0 +1,80 @@
+const Template = require('../../template');
+
+class Main extends Template {
+    constructor() {
+        super()
+        this.title = "京东年货超级盒子"
+        this.cron = "12 0,22 * * *"
+        this.task = 'local'
+        this.thread = 6
+    }
+
+    async prepare() {
+        this.linkId = 'Ll3Qb2mhCXSEWxruhv8qIw'
+    }
+
+    async main(p) {
+        let cookie = p.cookie
+        let u = await this.curl({
+                'url': `https://api.m.jd.com/?functionId=superboxSupBoxHomePage&body={"taskId":"332","linkId":"${this.linkId}","encryptPin":""}&_t=1635586254297&appid=activities_platform`,
+                // 'form':``,
+                cookie
+            }
+        )
+        let pin = u.data.encryptPin;
+        let gifts = []
+        u = await this.curl({
+                'url': `https://api.m.jd.com/?functionId=superboxSupBoxHomePage&body={"taskId":"332","linkId":"${this.linkId}","encryptPin":"${pin}"}&_t=1635586254297&appid=activities_platform`,
+                // 'form':``,
+                cookie: this.cookies[this.task][p.index + 1] || this.cookies[this.task][0]
+            }
+        )
+        let s = await this.curl({
+            url: `https://api.m.jd.com/?functionId=apTaskList&body={\"linkId\":\"${this.linkId}\",\"encryptPin\":\"\"}&_t=1635133026504&appid=activities_platform`,
+            cookie
+        })
+        for (let i of s.data) {
+            if (i.taskLimitTimes>1 && i.taskLimitTimes != i.taskDoTimes) {
+                let ss = await this.curl({
+                    'url': `https://api.m.jd.com/?functionId=apTaskDetail&body={"taskId":${i.id},"taskType":"BROWSE_SHOP","channel":4,"linkId":"${this.linkId}","encryptPin":""}&_t=1635133141666&appid=activities_platform`,
+                    cookie
+                })
+                for (let j of ss.data.taskItemList) {
+                    let sss = await this.curl({
+                        'url': 'https://api.m.jd.com/',
+                        'form': `functionId=apDoTask&body={"taskId":${i.id},"taskType":"BROWSE_SHOP","channel":4,"itemId":"${j.itemId}","linkId":"${this.linkId}","encryptPin":""}&_t=1635133142014&appid=activities_platform`
+                        , cookie
+                    })
+                    console.log(sss.data?.awardInfo);
+                }
+            }
+        }
+        for (let i of Array(20)) {
+            let ssss = await this.curl({
+                'url': `https://api.m.jd.com/?functionId=superboxOrdinaryLottery&body={"linkId":"${this.linkId}","encryptPin":""}&_t=1635134002941&appid=activities_platform`,
+                cookie
+            })
+            // console.log(ssss.data || ssss);
+            if (this.haskey(ssss, 'data.discount')) {
+                gifts.push(ssss.data.discount)
+            }
+            if (ssss.errMsg == '没有抽奖次数') {
+                break
+            }
+        }
+        let c = await this.curl({
+            'url': `https://api.m.jd.com/?functionId=superboxRealBigLottery&body={"linkId":"${this.linkId}","encryptPin":""}&_t=1635587400273&appid=activities_platform`,
+            cookie
+        })
+        console.log('任务满抽奖', c.data)
+        if (this.haskey(c, 'data.discount')) {
+            gifts.push(c.data.discount)
+        }
+        if (gifts.length) {
+            console.log(p.user, `活动奖励: ${this.sum(gifts)}元`)
+            this.notices(`活动奖励: ${this.sum(gifts)}元`, p.user)
+        }
+    }
+}
+
+module.exports = Main;
