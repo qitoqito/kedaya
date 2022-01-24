db1ef5792e53490da2ce0c619d10ae5e3d837577
air <92066902+qitoqito@users.noreply.github.com>
2022-01-24 11:25:49 +0800

    this.help支持指定助力用户

--- parse/jd/jd_help_sendBean.js
+++ parse/jd/jd_help_sendBean.js
@@ -6,9 +6,10 @@ class Main extends Template {
         this.title = "京东送豆得豆"
         this.cron = "1,11 0,9 * * *"
         this.help = 'main'
-        this.task = 'all'
+        this.task = 'own'
         this.verify = 1
         this.thread = 6
+        this.turn = 2
     }
 
     async prepare() {
@@ -51,7 +52,8 @@ class Main extends Template {
                                 'appId': this.source.data.appId,
                                 'rewardRecordId': this.source.data.rewardRecordId,
                                 'activityId': k.activeId,
-                                'activityCode': activityCode
+                                'activityCode': activityCode,
+                                count: this.source.data.activityInfo.inviteBeanQuantity
                             });
                         }
                     }
@@ -67,7 +69,7 @@ class Main extends Template {
         }
     }
 
-    async main(p) {
+    async assist(p) {
         this.options.headers.lks = this.md5(`${this['jd_invokeKey']}${this.timestamp}${p.inviter.activityCode}`)
         try {
             await this.curl({
@@ -84,16 +86,40 @@ class Main extends Template {
         }
     }
 
+    async main(p) {
+        let cookie = p.cookie
+        this.options.headers.lks = this.md5(`${this['jd_invokeKey']}${this.timestamp}`)
+        let m = await this.curl({
+                'url': `https://sendbeans.jd.com/common/api/bean/activity/myReward?itemsPerPage=10&currentPage=1&sendType=0&invokeKey=${this['jd_invokeKey']}`,
+                // 'form':``,
+                cookie
+            }
+        )
+        for (let i of this.haskey(m, 'datas')) {
+            if (i.status == 3) {
+                let r = await this.curl({
+                        'url': `https://sendbeans.jd.com/common/api/bean/activity/sendBean?rewardRecordId=${i.id}&invokeKey=${this['jd_invokeKey']}`,
+                        // 'form':``,
+                        cookie
+                    }
+                )
+                console.log(p.user, '领取助力京豆', r.success)
+            }
+        }
+        console.log(p.user, 'ok')
+    }
+
     async extra() {
         if (this.shareCode.length>0) {
             for (let i of this.shareCode) {
                 try {
                     let zlCookie = this.findValue(this.cookies['help'], encodeURIComponent(i.userPin));
+                    this.options.headers.lks = this.md5(`${this['jd_invokeKey']}${this.timestamp}${i.activityCode}`)
                     let rewardUrl = `https://sendbeans.jd.com/common/api/bean/activity/sendBean?activityCode=${i.activityCode}&rewardRecordId=${i.rewardRecordId}&jdChannelId=&userSource=mp&appId=wxccb5c536b0ecd1bf&invokeKey=${this['jd_invokeKey']}`
                     let s = await this.curl({url: rewardUrl, cookie: zlCookie})
                     console.log(s)
                     if (s.successs) {
-                        this.notices("获得送豆奖励", p.user)
+                        this.notices(`获得送豆奖励: ${i.count}`, p.user)
                     }
                 } catch (e) {
                     console.log(e.message)
--- parse/jd/jd_task_test.js
+++ parse/jd/jd_task_test.js
@@ -1,5 +1,4 @@
 const Template = require('../../template');
-
 class Main extends Template {
     constructor() {
         super()
@@ -7,27 +6,34 @@ class Main extends Template {
         // this.cron = "12 0,13 * * *"
         this.help = 2
         this.task = 'test'
-        this.readme= "这里是测试信息"
+        this.aid = 'main'
+        this.readme = "这里是测试信息"
+        this.turn = 2
     }
-
     async prepare() {
         let s = await this.curl({
             'url': 'https://wq.jd.com/activep3/singjd/queryexpirejingdou?_=1637926089761&g_login_type=0&callback=jsonpCBKC&g_tk=353098972&g_ty=ls&sceneval=2&g_login_type=1',
         })
         console.log("JSONP测试,如果输出字典,框架正常")
-        console.log(s,"\n")
+        console.log(s, "\n")
         console.log("JSON测试,如果输出字典,框架正常")
-        let s2=await this.curl({
-            url:'https://api.m.jd.com/client.action?functionId=queryMaterialProducts&client=wh5'
+        let s2 = await this.curl({
+            url: 'https://api.m.jd.com/client.action?functionId=queryMaterialProducts&client=wh5'
         })
         console.log(s2)
-
+        for (let cookie of this.cookies['help']) {
+            this.shareCode.push({
+                user: this.userName(cookie)
+            })
+        }
     }
-
+    async assist(p) {
+        console.log(`当前用户`, p.user, '当前助力', p.inviter.user)
+    }
     async main(p) {
         let cookie = p.cookie;
-        this.notices('框架测试',p.user)
+        console.log(p.user, '框架测试')
+        this.notices('框架测试', p.user)
     }
 }
-
 module.exports = Main;
