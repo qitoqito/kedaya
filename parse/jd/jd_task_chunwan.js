e28376d96c6a504e8ea426ef6d08f3c7f6b1029e
air <92066902+qitoqito@users.noreply.github.com>
2022-02-10 21:38:56 +0800

    Create jd_task_chunwan.js

---
+++ parse/jd/jd_task_chunwan.js
@@ -0,0 +1,15 @@
+const Template = require('../../template');
+class Main extends Template {
+    constructor() {
+        super()
+        this.title = "京东春晚"
+        // this.cron = "12 0,13 * * *"
+        this.help = 2
+        this.task = 'test'
+    }
+    async prepare() {
+    }
+    async main(p) {
+    }
+}
+module.exports = Main;
