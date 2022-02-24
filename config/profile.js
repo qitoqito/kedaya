5e5bf801b3a6f0fe6a0fd7886e83a68a12dbf4f9
air <92066902+qitoqito@users.noreply.github.com>
2022-02-24 13:49:02 +0800

    Update profile.js

--- config/profile.js
+++ config/profile.js
@@ -2,5 +2,6 @@ module.exports = {
     keywords: ['model', 'task', 'thread', 'help', 'exit', 'limit', 'except', 'reward', 'loop', 'interval', 'cron', 'expand', 'custom', 'turn', 'endTime', 'send', 'work', 'msgNum', 'msgTotal', 'msgExcept', 'msgWork', 'count'],
     userRegular: {
         'jd': /pin=([^;]+)/
-    }
+    },
+    thread: 3,
 }
