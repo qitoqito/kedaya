a79ed7c4a0203851ee29451491c9f5b7b6e5e3b3
air <92066902+qitoqito@users.noreply.github.com>
2022-02-25 19:06:44 +0800

    Update jd_task_addCart.js

--- parse/jd/jd_task_addCart.js
+++ parse/jd/jd_task_addCart.js
@@ -22,15 +22,28 @@ class Main extends Template {
                 })
             }
             else {
-                if (i.includes('=')) {
-                    s = this.match(/\s*([^\=]+)\s*=\s*(\w{32})/, i)
-                    if (s) {
-                        this.shareCode.push({
-                            host: s[0].includes('isvjcloud.com') ? s[0] : `${s[0]}.isvjcloud.com`,
-                            activityId: s[1]
-                        })
-                    }
+                s = this.match(/\s*([^\=]+)\s*=\s*(\w{32})/, i)
+                if (s) {
+                    this.shareCode.push({
+                        host: s[0].includes('isvjcloud.com') ? s[0] : `${s[0]}.isvjcloud.com`,
+                        activityId: s[1]
+                    })
                 }
+            }
+        }
+        for (let i of this.shareCode) {
+            let h = await this.response({
+                    'url': `https://${i.host}/wxCollectionActivity/activity2/${i.activityId}?activityId=${i.activityId}`,
+                }
+            )
+            let shop = await this.curl({
+                    'url': `https://${i.host}/wxCollectionActivity/shopInfo`,
+                    'form': `activityId=${i.activityId}`,
+                    cookie: h.cookie
+                }
+            )
+            if (this.haskey(shop, 'data.sid')) {
+                i.venderId = shop.data.sid
             }
         }
     }
@@ -39,6 +52,13 @@ class Main extends Template {
         let cookie = p.cookie;
         let host = p.inviter.host
         let activityId = p.inviter.activityId
+        if (p.inviter.venderId) {
+            let follow = await this.curl({
+                'url': 'https://api.m.jd.com/client.action?g_ty=ls&g_tk=518274330',
+                'form': `functionId=followShop&body={"follow":"true","shopId":"${p.inviter.venderId}","award":"true","sourceRpc":"shop_app_home_follow"}&osVersion=13.7&appid=wh5&clientVersion=9.2.0&loginType=2&loginWQBiz=interact`,
+                cookie: p.cookie
+            })
+        }
         let isvObfuscator = await this.curl({
             url: 'https://api.m.jd.com/client.action',
             form: 'functionId=isvObfuscator&body=%7B%22id%22%3A%22%22%2C%22url%22%3A%22https%3A%2F%2Fddsj-dz.isvjcloud.com%22%7D&uuid=5162ca82aed35fc52e8&client=apple&clientVersion=10.0.10&st=1631884203742&sv=112&sign=fd40dc1c65d20881d92afe96c4aec3d0',
@@ -55,7 +75,19 @@ class Main extends Template {
                 cookie: h.cookie
             }
         )
-        let venderId = info.content.data.venderId
+        try {
+            var venderId = info.content.data.venderId
+        } catch (e) {
+            let shop = await this.curl({
+                    'url': `https://${host}/wxCollectionActivity/shopInfo`,
+                    'form': `activityId=${activityId}`,
+                    cookie: h.cookie
+                }
+            )
+            if (this.haskey(shop, 'data.sid')) {
+                var venderId = shop.data.sid
+            }
+        }
         let getPin = await this.response({
                 'url': `https://${host}/customer/getMyPing`,
                 form: `userId=${venderId}&token=${isvObfuscator.token}&fromType=APP`,
@@ -63,7 +95,7 @@ class Main extends Template {
             }
         )
         if (!this.haskey(getPin, 'content.data.secretPin')) {
-            console.log(`可能是黑号,停止运行`)
+            console.log(`可能是黑号或者黑ip,停止运行`)
             return
         }
         let member = await this.response({
@@ -98,6 +130,11 @@ class Main extends Template {
         else {
             console.log(getPrize.errorMessage);
         }
+        await this.curl({
+            'url': 'https://api.m.jd.com/client.action?g_ty=ls&g_tk=518274330',
+            'form': `functionId=followShop&body={"follow":"false","shopId":"${p.inviter.venderId}","award":"true","sourceRpc":"shop_app_home_follow"}&osVersion=13.7&appid=wh5&clientVersion=9.2.0&loginType=2&loginWQBiz=interact`,
+            cookie: p.cookie
+        })
     }
 }
 
