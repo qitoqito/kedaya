91e66a8ed9832b86c98ae69e3ac1458995b51dc1
air <92066902+qitoqito@users.noreply.github.com>
2022-03-12 09:57:33 +0800

    双重判断是否过期,避免误判

--- parse/jd/jd_task_checkCookie.js
+++ parse/jd/jd_task_checkCookie.js
@@ -11,12 +11,23 @@ class Main extends Template {
     }
 
     async main(p) {
-        p.url = 'https://plogin.m.jd.com/cgi-bin/ml/islogin'
-        let login = await this.curl(p);
-        console.log(p.user, login.islogin == '0' ? "账号过期了呀🐶" : "账号还没过期呢🍀")
-        if (this.source.islogin == '0') {
-            this.notices("账户过期", p.user)
-            this.code.push(p.user)
+        let cookie = p.cookie
+        let s = await this.curl({
+                'url': `https://plogin.m.jd.com/cgi-bin/ml/islogin`,
+                // 'form':``,
+                cookie
+            }
+        )
+        let s2 = await this.curl({
+                'url': `https://wq.jd.com/bases/orderlist/list?order_type=3&start_page=1&page_size=10&last_page=0&callersource=mainorder&traceid=1173839808217437357&t=1619852939462&sceneval=2&g_ty=ls&g_tk=5381`,
+                cookie
+            }
+        )
+        if ((s.islogin == '0' && s2.errCode == '13')) {
+            this.notices("账号过期了呀🐶", p.user) 
+        }
+        else {
+            console.log("账号还没过期呢🍀")
         }
     }
 }
