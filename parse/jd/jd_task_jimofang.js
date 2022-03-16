5080ffbe0f92f30ee0d65da637fbf9b5eecdc712
air <92066902+qitoqito@users.noreply.github.com>
2022-03-17 00:10:21 +0800

    Update jd_task_jimofang.js

--- parse/jd/jd_task_jimofang.js
+++ parse/jd/jd_task_jimofang.js
@@ -4,27 +4,59 @@ class Main extends Template {
     constructor() {
         super()
         this.title = "京东集魔方赢大奖"
-        // this.cron = "22 0,22 * * *"
-        this.task = 'local'
-        this.thread = 6
+        this.cron = "22 0,22 * * *"
+        this.task = 'zong'
+        this.thread = 3
+        this.verify = 1
     }
 
     async prepare() {
-        let h = await this.curl({
-                'url': `https://prodev.m.jd.com/mall/active/TqTRGRrp9HZTfeyRTL2UGmX4mHG/index.html`,
+        this.code = [
+            `https://prodev.m.jd.com/mall/active/2GaAnLLb7NcHkEjPXphja7SSQ7Z4/index.html`
+        ]
+        let custom = this.getValue('custom')
+        let expand = this.getValue('expand')
+        if (expand.length) {
+            for (let i of expand) {
+                this.code.unshift(i)
             }
-        )
-        let pageId = this.match(/"pageId"\s*:\s*"(\d+)"/, h)
-        let activityId = this.match(/"activityId"\s*:\s*"(\d+)"/, h)
-        console.log(pageId)
-        console.log(activityId)
-        let s = await this.curl({
-                'url': `https://api.m.jd.com/client.action?client=wh5&clientVersion=10.3.0&osVersion=15.1.1&networkType=wifi&ext=%7B%22prstate%22:%220%22%7D&functionId=qryCompositeMaterials&t=1640923295510&body=%7B%22geo%22:%7B%22lng%22:%22129.61221885577431%22,%22lat%22:%2293.966160894469766%22%7D,%22mcChannel%22:0,%22activityId%22:%22${activityId}%22,%22pageId%22:%22${pageId}%22,%22qryParam%22:%22[%7B%5C%22type%5C%22:%5C%22advertGroup%5C%22,%5C%22id%5C%22:%5C%2206066757%5C%22,%5C%22mapTo%5C%22:%5C%22advData%5C%22,%5C%22next%5C%22:[%7B%5C%22type%5C%22:%5C%22productGroup%5C%22,%5C%22mapKey%5C%22:%5C%22desc%5C%22,%5C%22mapTo%5C%22:%5C%22productGroup%5C%22,%5C%22attributes%5C%22:13%7D]%7D]%22,%22applyKey%22:%2221new_products_h%22%7D`,
+        }
+        if (custom.length) {
+            this.code = []
+            for (let i of custom) {
+                this.code.push(i)
             }
-        )
-        let data = this.dumps(s)
-        this.dict.advertId = this.matchAll(/"advertId"\s*:\s*"(\d+)"/g, data)
-        this.dict.skuId = this.matchAll(/"skuId"\s*:\s*"(\d+)"/g, data)
+        }
+        let urls = []
+        for (let i of this.code) {
+            let url = i.substring(0, 4) == 'http' ? i : `https://prodev.m.jd.com/mall/active/${i}/index.html`
+            if (!urls.includes(url)) {
+                urls.push(url)
+            }
+        }
+        if (urls.length) {
+            for (let url of urls) {
+                try {
+                    let h = await this.curl({
+                            url
+                        }
+                    )
+                    let pageId = this.match(/"pageId"\s*:\s*"(\d+)"/, h)
+                    let activityId = this.match(/"activityId"\s*:\s*"(\d+)"/, h)
+                    let s = await this.curl({
+                            'url': `https://api.m.jd.com/client.action?client=wh5&clientVersion=10.3.0&osVersion=15.1.1&networkType=wifi&ext=%7B%22prstate%22:%220%22%7D&functionId=qryCompositeMaterials&t=1640923295510&body={"geo":null,"mcChannel":0,"activityId":"${activityId}","pageId":"${pageId}","qryParam":"[{\\"type\\":\\"advertGroup\\",\\"id\\":\\"06167705\\",\\"mapTo\\":\\"advData\\",\\"next\\":[{\\"type\\":\\"productGroup\\",\\"mapKey\\":\\"comment[0]\\",\\"mapTo\\":\\"productGroup\\",\\"attributes\\":13},{\\"type\\":\\"productGroup\\",\\"mapKey\\":\\"comment[1]\\",\\"mapTo\\":\\"productGroup2\\",\\"attributes\\":13}]}]","applyKey":"21new_products_h"}`,
+                        }
+                    )
+                    let data = this.dumps(s)
+                    let advertId = this.matchAll(/(\d+)/g, this.matchAll(/"comments"\s*:\s*\[([^\]]+)\]/g, data).join(","))
+                    let skuId = this.matchAll(/"skuId"\s*:\s*"(\d+)"/g, data)
+                    this.shareCode.push({
+                        pageId, activityId, advertId, skuId
+                    })
+                } catch (e) {
+                }
+            }
+        }
     }
 
     async main(p) {
@@ -40,7 +72,7 @@ class Main extends Template {
         for (let i of l.result.taskPoolInfo.taskList) {
             if (!i.taskStatus) {
                 for (let j of Array(i.toastTime + 5)) {
-                    let sku = this.random(this.dict.skuId || [
+                    let sku = this.random(p.inviter.skuId || [
                         '10025690385788',
                         '100029081460',
                         '100027604000',
@@ -54,31 +86,28 @@ class Main extends Template {
                         '100023781630',
                         '100018818732',
                     ], 1)
-                    let advertId = this.random([
-                        '4901613044', '4901611735',
-                        '4901613078', '4901610839',
-                        '4901613590', '4901613039',
-                        '4901613040', '4901613043',
-                        '4901613045', '4901613041',
-                        '4901613042'
+                    let advertId = this.random(p.inviter.activeId || [
+                        "18395215", "18468845", "18395285", "18470991", "18395381", "18468452", "18396456", "18472112", "18451573", "18468940", "18460143", "18471960"
                     ], 1)
                     let s = await this.curl({
                             'url': `https://api.m.jd.com/client.action?uuid=&client=wh5&clientVersion=10.3.0&osVersion=15.1.1&networkType=wifi&&appid=content_ecology&functionId=executeNewInteractionTask&t=1640607957804&body={"geo":{"lng":"","lat":""},"mcChannel":0,"sign":3,"interactionId":${interactionId},"taskPoolId":${taskPoolId},"taskType":${i.taskId},"sku":"${sku}","advertId":"${advertId}"}`,
-                            // 'form':``,
                             cookie
                         }
                     )
-                    console.log(s.result?.lotteryInfoList || s.result)
                     if (this.haskey(s, 'result.lotteryInfoList')) {
                         let gift = this.column(s.result.lotteryInfoList, 'quantity', 'name')
                         if (this.dumps(gift) != '{}') {
-                            this.notices(`获得奖励${this.dumps(gift)}`, p.user)
+                            this.notices(`${this.dumps(gift)}`, p.user)
+                            console.log(`获得奖励: ${this.dumps(gift)}`, p.user)
                         }
+                    }
+                    else {
+                        console.log("什么也没有", p.user)
                     }
                 }
             }
             else {
-                console.log("任务已经完成了")
+                console.log("任务已经完成了", p.user)
             }
         }
         let lo = await this.curl({
@@ -91,7 +120,8 @@ class Main extends Template {
             console.log(lo?.result?.lotteryInfoList)
             let gift = this.column(lo.result.lotteryInfoList, 'quantity', 'name')
             if (this.dumps(gift) != '{}') {
-                this.notices(`抽奖获得奖励${this.dumps(gift)}`, p.user)
+                console.log(`${this.dumps(gift)}`, p.user)
+                this.notices(`${this.dumps(gift)}`, p.user)
             }
         }
         else {
