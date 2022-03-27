4ff60be9095f19da6175853d75a354d50f941568
air <92066902+qitoqito@users.noreply.github.com>
2022-03-27 14:53:58 +0800

    添加proxy代理
    添加proxy代理
    filename_proxy=(http|https|sock)://ip:port

--- config/profile.js
+++ config/profile.js
@@ -1,7 +1,8 @@
 module.exports = {
-    keywords: ['model', 'task', 'thread', 'help', 'exit', 'limit', 'except', 'reward', 'loop', 'interval', 'cron', 'expand', 'custom', 'turn', 'endTime', 'send', 'work', 'msgNum', 'msgTotal', 'msgExcept', 'msgWork', 'count'],
+    keywords: ['model', 'task', 'thread', 'help', 'exit', 'limit', 'except', 'reward', 'loop', 'interval', 'cron', 'expand', 'custom', 'turn', 'endTime', 'send', 'work', 'msgNum', 'msgTotal', 'msgExcept', 'msgWork', 'count', 'proxy'],
     userRegular: {
         'jd': /pin=([^;]+)/,
         'kejiwanjia': [/pin=([^;]+)/, /username=([^;]+)/],
+        'baidu': /pin=([^;]+)/,
     },
 }
