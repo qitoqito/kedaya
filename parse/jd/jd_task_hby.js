c8f995f0d379d55f0259add81d48b1d6c91815b5
air <92066902+qitoqito@users.noreply.github.com>
2022-04-14 21:03:26 +0800

    Update jd_task_hby.js

--- parse/jd/jd_task_hby.js
+++ parse/jd/jd_task_hby.js
@@ -4,9 +4,9 @@ class Main extends Template {
     constructor() {
         super()
         this.title = "京东红包雨"
-        // this.cron = "1 19-23 * * *"
+        this.cron = "1 0,10,14,16,18,20 * * *"
         this.task = 'local'
-        this.thread = 6
+        this.thread = 3
     }
 
     async prepare() {
@@ -24,10 +24,10 @@ class Main extends Template {
                         url = `https://prodev.m.jd.com/mall/active/${this.custom}/index.html`
                     }
                     let s = await this.curl({
-                            'url': `https://prodev.m.jd.com/mall/active/${this.custom}/index.html`,
+                            'url': url,
                         }
                     )
-                    let babelProjectId = this.match(/"activityId"\s*:\s*"(\d+)"/s)
+                    let babelProjectId = this.match(/"activityId"\s*:\s*"(\d+)"/, s)
                     let babelPageId = this.match(/"pageId"\s*:\s*"(\d+)"/, s)
                     if (babelPageId && babelProjectId) {
                         body = {babelProjectId, babelPageId}
@@ -39,7 +39,7 @@ class Main extends Template {
             }
         }
         if (this.dumps(body) == '{}') {
-            body = {"babelProjectId": "01142214", "babelPageId": "3351872"}
+            body = {"babelProjectId": "01210019", "babelPageId": "3484113"}
         }
         this.dict = body
         console.log('当前红包雨:', this.dumps(this.dict))
