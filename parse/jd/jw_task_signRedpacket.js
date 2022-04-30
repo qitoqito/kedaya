2cd472386212a54e3fb340a871d8102a10deb403
air <92066902+qitoqito@users.noreply.github.com>
2022-04-30 20:27:17 +0800

    Update jw_task_signRedpacket.js

--- parse/jd/jw_task_signRedpacket.js
+++ parse/jd/jw_task_signRedpacket.js
@@ -5,23 +5,29 @@ class Main extends Template {
         super()
         this.title = "京东微信签到红包"
         this.cron = "32 0,21 * * *"
-        this.thread = 6
+        // this.thread = 6
         this.task = 'local'
-        this.header = {
-            'content-type': 'application/json',
-            'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15F79 MicroMessenger/8.0.15(0x18000f2e) NetType/WIFI Language/zh_CN',
-            'referer': 'https://servicewechat.com/wx91d27dbf599dff74/581/page-frame.html',
-        }
+        this.import = ['jdAlgo']
     }
 
     async prepare() {
+        this.algo = new this.modules.jdAlgo()
+        this.algo.set({
+            'appId': '9a38a',
+            'type': 'wechat',
+            'fp': '5727941528525264'
+        })
     }
 
     async main(p) {
-        let cookie = p.cookie;
-        let s = await this.curl({
-                'url': `https://api.m.jd.com/signTask/doSignTask?functionId=SignComponent_doSignTask&appid=hot_channel&loginWQBiz=signcomponent&loginType=2&body={"activityId":"10002"}&g_ty=ls&g_tk=1294369933`,
-                cookie
+        let cookie = p.cookie; 
+        let s = await this.algo.curl({
+                'url': `https://api.m.jd.com/signTask/doSignTask?client=apple&clientVersion=7.16.280&functionId=SignComponent_doSignTask&appid=hot_channel&loginWQBiz=signcomponent&loginType=2&body={"activityId":"10002"}`,
+                cookie,
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15F79 MicroMessenger/8.0.15(0x18000f2e) NetType/WIFI Language/zh_CN',
+                    referer: 'https://servicewechat.com/wx91d27dbf599dff74/616/page-frame.html'
+                }
             }
         )
         if (this.haskey(s, 'data.rewardValue')) {
