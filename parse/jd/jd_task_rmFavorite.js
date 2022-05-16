1e694c806ed50fbac6f0e20d58b983b1e67b9e0d
air <92066902+qitoqito@users.noreply.github.com>
2022-05-16 17:08:41 +0800

    Update jd_task_rmFavorite.js

--- parse/jd/jd_task_rmFavorite.js
+++ parse/jd/jd_task_rmFavorite.js
@@ -15,28 +15,33 @@ class Main extends Template {
 
     async main(p) {
         let cookie = p.cookie;
-        let count = parseInt(this.dict.count || 20)
+        let count = parseInt(this.profile.count || this.dict.count || 20)
+        let page = 1
         for (let i = 0; i<count / 20; i++) {
             let s = await this.curl({
-                    'url': `https://m.jingxi.com/fav/comm/FavCommQueryFilter?cp=1&pageSize=20&category=0&promote=0&cutPrice=0&coupon=0&stock=0&_=1650625973463&&sceneval=2&g_login_type=1&callback=aaa`,
+                    'url': `https://m.jingxi.com/fav/comm/FavCommQueryFilter?cp=${page}&pageSize=20&category=0&promote=0&cutPrice=0&coupon=0&stock=0&_=1650625973463&&sceneval=2&g_login_type=1&callback=aaa`,
                     cookie
                 }
             )
             if (this.haskey(s, 'data')) {
+                // console.log(s.data)
                 console.log("当前商品收藏数:", s.totalNum)
                 if (s.totalNum>0) {
+                    page = Math.ceil(s.totalNum / 20)>1 ? this.rand(1, Math.ceil(s.totalNum / 20)) : 1
                     // commTitle
-                    if (this.dict.whiteList) {
-                        let whiteList = this.dict.whiteList.split(",").join('|')
+                    if (this.profile.whiteList || this.dict.whiteList) {
+                        let whiteList = (this.profile.whiteList || this.dict.whiteList).split(",").join('|')
                         var data = s.data.filter(d => !d.commTitle.match(whiteList))
                     }
-                    else if (this.dict.blackList) {
-                        let blackList = this.dict.blackList.split(",").join('|')
+                    else if (this.profile.blackList || this.dict.blackList) {
+                        let blackList = (this.profile.blackList || this.dict.blackList).blackList.split(",").join('|')
                         var data = s.data.filter(d => d.commTitle.match(blackList))
                     }
                     else {
                         var data = s.data
                     }
+                    // console.log(s.data)
+                    // console.log(this.column(s.data, 'commTitle').join("|"))
                     if (data.length>0) {
                         console.log(`正在删除:`, this.column(data, 'commTitle').join("|"))
                         let rm = await this.curl({
@@ -45,7 +50,8 @@ class Main extends Template {
                             }
                         )
                         console.log(rm.resultMsg)
-                    }else{
+                    }
+                    else {
                         break
                     }
                 }
@@ -56,6 +62,7 @@ class Main extends Template {
             }
             else {
                 console.log("可能黑ip了,没有获取到数据")
+                break
             }
         }
     }
