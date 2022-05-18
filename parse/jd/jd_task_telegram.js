a5cbb724eaf1d4a0df1fc9a1623c748dfba756e8
air <92066902+qitoqito@users.noreply.github.com>
2022-05-18 19:51:37 +0800

    Update jd_task_telegram.js

--- parse/jd/jd_task_telegram.js
+++ parse/jd/jd_task_telegram.js
@@ -3,15 +3,152 @@ const Template = require('../../template');
 class Main extends Template {
     constructor() {
         super()
-        this.title = "京东telegram监听"
-        // this.cron = "12 0,13 * * *"
-        // this.help = 2
-        this.task = 'test'
+        this.title = "telegram监控"
+        this.import = ['fs', 'node-telegram-bot-api']
         this.jump = 1
-        this.import = ['node-telegram-bot-api', 'jdUrl']
     }
 
     async prepare() {
+        if (this.custom) {
+            await this.tg()
+        }
+        else {
+            await this.tgMsg()
+        }
+    }
+
+    async tgMsg() {
+        if (this['QITOQITO_MAP']) {
+            let change = {}
+            for (let k of this['QITOQITO_MAP'].replace(/\&/g, "\|").split("|")) {
+                let a = k.split("=")
+                for (let i of a[0].split(',')) {
+                    change[i] = {
+                        map: a[1],
+                        type: a[1].split("_")[0]
+                    }
+                }
+            }
+            this.dict.map = change
+        }
+        this.TelegramBot = this.modules['node-telegram-bot-api']
+        let request = {}
+        if (this.profile.BOT_PROXY) {
+            if (this.profile.BOT_PROXY.toLowerCase().includes("socks")) {
+                var SocksProxyAgent = require('socks-proxy-agent');
+                var agent = new SocksProxyAgent(this.profile.BOT_PROXY.toLowerCase());
+                request.agent = agent
+            }
+            else {
+                request.proxy = this.profile.BOT_PROXY
+            }
+        }
+        this.bot = new this.TelegramBot(this.profile.BOT_TOKEN, {
+            polling: true, request,
+        });
+        this.bot.on('text', async (msg) => {
+            let timestamp = parseInt(new Date().getTime() / 1000)
+            let chat = msg.chat
+            let from = msg.from
+            let messageId = msg.message_id
+            let chatId = chat.id
+            let text = msg.text
+            let group = ['group', 'supergroup'].includes(msg.chat.type)
+            let id = from.id
+            let admin = this.profile.BOT_ROOT.includes(id.toString())
+            let ban = 0
+            let groupTask = 0
+            var reText
+            if (this.haskey(msg, 'reply_to_message')) {
+                reText = msg.reply_to_message.text
+            }
+            if (this.n == 0) {
+                this.sendMessage(chat.id, `我出来溜达了哦`, '', 23333);
+                this.n++
+            }
+            if (admin) {
+                if (text == "上线") {
+                    console.log(`我还在呢`)
+                    this.sendMessage(chatId, `我还在呢`, '', 23333)
+                }
+                if (text == "下线") {
+                    console.log(`不要捣乱哦,小心铁锅炖你`)
+                    this.sendMessage(chatId, `不要捣乱哦,小心铁锅炖你`, '', 23333)
+                }
+                let custom = this.profile.TASK_CUSTOM.split(',').join("|")
+                let script = this.profile.TASK_SCRIPT.split(',').join("|")
+                // let map = Object.keys(this.profile.TASK_MAP)
+                let taskMap = {}
+                for (let kk in this.profile) {
+                    if (kk.includes('TASK_MAP_')) {
+                        taskMap[kk.replace('TASK_MAP_', '')] = this.profile[kk]
+                    }
+                }
+                let map = Object.keys(taskMap)
+                if (this.match(new RegExp(`(^${map.join("|")})`), text)) {
+                    for (let i of map) {
+                        if (this.match(new RegExp(`(^${i})`), text)) {
+                            let k = text.split(i)
+                            text = `task ${taskMap[i]} -custom ${k[1]}`
+                            break
+                        }
+                    }
+                }
+                console.log(text)
+                if (this.match(new RegExp(`(^${custom})`), text) && !this.match(/task\s*\w+/, text)) {
+                    let command = this.match(new RegExp(`(^${custom})`), text)
+                    text = `task jd_task_${command} -custom ${reText || text.replace(command, '')}`
+                }
+                else if (this.match(new RegExp(`(^${script}$)`), text) && !this.match(/task\s*\w+/, text)) {
+                    let command = this.match(new RegExp(`(^${script})`), text)
+                    text = `task jd_task_${command}`
+                }
+                else if (this.dict.map && this.match(new RegExp(`(^${Object.keys(this.dict.map).join("|")})`), text) && !this.match(/task\s*\w+/, text)) {
+                    let command = this.match(new RegExp(`(^${Object.keys(this.dict.map).join("|")})`), text)
+                    text = `task ${this.dict.map[command].map} -custom ${reText || text.replace(command, '')}`
+                }
+                else if (this.match(/^ini\s*-\w+/, text)) {
+                    text = text.replace("ini", 'task jd_task_ini')
+                }
+                let filename = this.match(/task\s*(\w+)\s*/, text)
+                if (filename) {
+                    try {
+                        console.log(`即将执行:`, filename)
+                        let argv = text.split(filename)
+                        try {
+                            let type = filename.split('_')[0]
+                            switch (type) {
+                                case "js":
+                                case "jx":
+                                case "jr":
+                                case 'jw':
+                                    type = 'jd'
+                                    break
+                            }
+                            let yaya = require(`${this.dirname}/parse/${type}/${filename}`)
+                            let kedaya = new yaya()
+                            let echo = `执行命令: ${kedaya.title}\n执行脚本: ${filename}\n执行参数: ${argv[1]}`
+                            this.sendMessage(chatId, echo, '', 6666)
+                            let params = this.query(argv[1].replace(/\s*-(\w+)\s+/g, `&$1=`), '&', 1)
+                            let pp = {
+                                filename
+                            }
+                            for (let i in params) {
+                                if (params[i]) {
+                                    pp[i] = params[i]
+                                }
+                            }
+                            await kedaya.init(params)
+                        } catch (e) {
+                        }
+                    } catch (e) {
+                    }
+                }
+            }
+        })
+    }
+
+    async tg() {
         this.locationId = {}
         this.ban = 0
         for (let i of this.getValue('custom')) {
@@ -54,10 +191,6 @@ class Main extends Template {
         this.bot = new this.TelegramBot(this.dict.token, {
             polling: true, request,
         });
-        await this.tg()
-    }
-
-    async tg() {
         let isActive = 0
         this.bot.on('text', async (msg) => {
             // console.log(msg)
@@ -114,7 +247,6 @@ class Main extends Template {
                     text = text.replace("ini", 'task jd_task_ini')
                 }
                 let filename = this.match(/task\s*(\w+)\s*/, text)
-
                 if (filename && admin) {
                     console.log(filename)
                     try {
@@ -151,13 +283,9 @@ class Main extends Template {
     }
 
     async sendMessage(id, echo, params = {}, timeout = 0) {
-        if (this.dict.except && this.dict.except.includes(id.toString())) {
-            console.log("id在排除名单里面,不发送消息")
-            return
-        }
         this.bot.sendMessage(id, echo, params || {}).then(async (res) => {
             if (timeout) {
-                await this.wait(16666)
+                await this.wait(6666)
                 console.log(`删除消息: ${echo}`)
                 this.bot.deleteMessage(res.chat.id, res.message_id)
             }
