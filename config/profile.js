b5ddbc699cb7b794c1960a02673ccefd9141bd17
air <92066902+qitoqito@users.noreply.github.com>
2022-06-02 12:08:53 +0800

    Update profile.js
    系统关键字:aid

--- config/profile.js
+++ config/profile.js
@@ -1,5 +1,5 @@
 module.exports = {
-    keywords: ['model', 'task', 'thread', 'help', 'exit', 'limit', 'except', 'reward', 'loop', 'interval', 'cron', 'expand', 'custom', 'turn', 'endTime', 'send', 'work', 'msgNum', 'msgTotal', 'msgExcept', 'msgWork', 'count', 'proxy', 'timeout', 'skip'],
+    keywords: ['model', 'task', 'thread', 'help', 'exit', 'limit', 'except', 'reward', 'loop', 'interval', 'cron', 'expand', 'custom', 'turn', 'endTime', 'send', 'work', 'msgNum', 'msgTotal', 'msgExcept', 'msgWork', 'count', 'proxy', 'timeout', 'skip', 'aid'],
     userRegular: {
         'jd': /pin=([^;]+)/,
         'kejiwanjia': [/pin=([^;]+)/, /username=([^;]+)/],
