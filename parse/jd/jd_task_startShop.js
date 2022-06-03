6b511869203d49a597e54a5a1fd262fb9df7b222
air <92066902+qitoqito@users.noreply.github.com>
2022-06-03 11:44:56 +0800

    Create jd_task_startShop.js

---
+++ parse/jd/jd_task_startShop.js
@@ -0,0 +1,41 @@
+const Template = require('../../template');
+
+class Main extends Template {
+    constructor() {
+        super()
+        this.title = "京东618超级盲盒"
+        this.cron = "12 15 */4 * *"
+        this.help = 2
+        this.task = 'local'
+    }
+
+    async prepare() {
+    }
+
+    async main(p) {
+        let cookie = p.cookie;
+        let s = await this.curl({
+                'url': `https://api.m.jd.com/?functionId=apTaskList&body={"linkId":"Jim-Gu6R_lyd4LT6nz69ow"}&_t=1654226708008&appid=activities_platform&cthr=1`,
+                // 'form':``,
+                cookie
+            }
+        )
+        this.assert(this.haskey(s, 'data'), '没有获取到数据')
+        for (let i of s.data) {
+            if (i.taskLimitTimes != i.taskDoTimes) {
+                let d = await this.curl({
+                        'url': `https://api.m.jd.com/`,
+                        'form': `functionId=apDoTask&body={"taskType":"${i.taskType}","taskId":${i.id},"linkId":"Jim-Gu6R_lyd4LT6nz69ow"}&_t=1654226707654&appid=activities_platform&cthr=1`,
+                        cookie
+                    }
+                )
+                console.log("正在执行:", i.taskTitle, this.haskey(d, 'data.finished'))
+            }
+            else {
+                console.log('任务已经完成:', i.taskTitle)
+            }
+        }
+    }
+}
+
+module.exports = Main;
