0bdc9290d7d1334438003dd4fec322e068ff6fbb
air <92066902+qitoqito@users.noreply.github.com>
2022-06-09 16:14:43 +0800

    Update jd_task_united.js

--- parse/jd/jd_task_united.js
+++ parse/jd/jd_task_united.js
@@ -8,14 +8,38 @@ class Main extends Template {
         this.task = 'local'
         this.import = ['jdUrl']
         this.verify = 1
-        // this.thread=3
+        // this.thread = 3
     }
 
     async prepare() {
         this.assert(this.profile.custom, '请正确填写custom')
         for (let i of this.unique(this.getValue('custom'))) {
+            let a = await this.curl({
+                    'url': `https://jinggengjcq-isv.isvjcloud.com/dm/front/openCardNew/shopProduct?mix_nick=BZwJHR`,
+                    json: {
+                        "jsonRpc": "2.0",
+                        "params": {
+                            "commonParameter": {
+                                "appkey": "51B59BB805903DA4CE513D29EC448375",
+                                "m": "POST",
+                                "timestamp": this.timestamp,
+                                "userId": "10299171"
+                            },
+                            "admJson": {
+                                "actId": "5e15292a037747739552fd2f12e_22060104",
+                                "userId": "10299171",
+                                "method": "/openCardNew/shopProduct",
+                            }
+                        }
+                    }
+                }
+            )
+            let skuList = []
+            if (this.haskey(a, 'data.data.cusShopProductList')) {
+                skuList = this.column(a.data.data.cusShopProductList, 'numId')
+            }
             this.shareCode.push({
-                actId: i
+                actId: i, skuList
             })
         }
     }
@@ -84,8 +108,9 @@ class Main extends Template {
             )
             for (let i of state.data.data) {
                 if (!i.isComplete) {
-                    if (['viewShop', 'viewGoods'].includes(i.type)) {
-                        console.log(i.missionName)
+                    let k = 0
+                    if (['viewShop', 'viewGoods', 'uniteAddCart', 'uniteCollectShop'].includes(i.type)) {
+                        console.log('正在运行:', i.missionName)
                         let json = {
                             "jsonRpc": "2.0",
                             "params": {
@@ -113,9 +138,17 @@ class Main extends Template {
                             let remark = this.haskey(s, 'data.data.remark') || ''
                             let num = this.match(/(\d+)个京豆/, remark)
                             if (num) {
+                                k = 0
                                 gifts += parseInt(num)
                             }
-                            console.log(remark)
+                            else {
+                                k++
+                            }
+                            console.log(remark || '什么也没有啊')
+                            if (k == 3) {
+                                console.log(`检测到没有豆子奖励,退出浏览`)
+                                break
+                            }
                             await this.wait(this.rand(1000, 2000))
                         }
                     }
@@ -129,6 +162,79 @@ class Main extends Template {
             }
             else {
                 console.log(`什么也没有`)
+            }
+            let skuList = p.inviter.skuList
+            if (skuList.length>0) {
+                skuList = skuList.map(d => d.toString())
+                let cart = await this.curl({
+                        'url': `https://api.m.jd.com/api?functionId=pcCart_jc_getCurrentCart&appid=JDC_mall_cart&body={}`,
+                        // 'form':``,
+                        cookie: p.cookie,
+                        headers: {
+                            "referer": "https://cart.jd.com/cart_index/",
+                            "user-agent": "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:98.0) Gecko/20100101 Firefox/98.0",
+                        }
+                    }
+                )
+                let skus = []
+                let packs = []
+                if (this.haskey(cart, 'resultData.cartInfo.vendors')) {
+                    for (let i of cart.resultData.cartInfo.vendors) {
+                        for (let j of this.haskey(i, 'sorted')) {
+                            if (this.haskey(j, 'item.items')) {
+                                if (j.item.items.length>0) {
+                                    for (let k of j.item.items) {
+                                        if (skuList.includes(k.item.Id)) {
+                                            packs.push(
+                                                {
+                                                    "num": k.item.Num.toString(),
+                                                    "ybPackId": j.item.promotionId,
+                                                    "sType": "11",
+                                                    "TheSkus": [{
+                                                        "num": k.item.Num.toString(),
+                                                        "Id": k.item.Id.toString()
+                                                    }],
+                                                    "Id": j.item.promotionId
+                                                }
+                                            )
+                                        }
+                                    }
+                                }
+                                else {
+                                    if (skuList.includes(j.item.Id)) {
+                                        skus.push(
+                                            {
+                                                "num": j.item.Num.toString(),
+                                                "Id": j.item.Id.toString(),
+                                                "skuUuid": j.item.skuUuid,
+                                                "useUuid": j.item.useUuid
+                                            }
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                console.log('即将删除购物车数目:', skus.length + packs.length)
+                if (skus.length>0 || packs.length>0) {
+                    let cartRemove = await this.curl({
+                            'url': `https://api.m.jd.com/api`,
+                            'form': `functionId=pcCart_jc_cartRemove&appid=JDC_mall_cart&body=${this.dumps({
+                                "operations": [{
+                                    "carttype": "4",
+                                    "TheSkus": skus,
+                                    "ThePacks": packs
+                                }], "serInfo": {}
+                            })}`,
+                            cookie: p.cookie,
+                            headers: {
+                                "referer": "https://cart.jd.com/cart_index/",
+                                "user-agent": "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:98.0) Gecko/20100101 Firefox/98.0",
+                            }
+                        }
+                    )
+                }
             }
         } catch (e) {
             console.log(e)
