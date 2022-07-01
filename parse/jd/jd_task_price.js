c4ed8c40219ab887f435d070be71d218966e8a83
air <92066902+qitoqito@users.noreply.github.com>
2022-07-01 22:29:37 +0800

    Update jd_task_price.js

--- parse/jd/jd_task_price.js
+++ parse/jd/jd_task_price.js
@@ -5,19 +5,17 @@ class Main extends Template {
         super()
         this.title = "京东保价"
         this.cron = "38 */10 * * *"
-        this.import = ['jsdom', 'jdAlgo']
-        // this.thread = 1
+        this.import = ['jdAlgo']
         this.task = 'local'
     }
 
     async prepare() {
-        // this.algo = new this.modules.jdAlgo()
-        // this.algo.set({
-        //     'appId': '3adb2',
-        //     // 'type': 'app',
-        //     'fp': '7052439682005356',
-        // })
-        await this.waap()
+        this.algo = new this.modules.jdAlgo()
+        this.algo.set({
+            'appId': 'd2f64',
+            'type': 'web',
+            "version": "3.1",
+        })
     }
 
     async main(p) {
@@ -30,24 +28,19 @@ class Main extends Template {
         let s = {}
         for (let i = 0; i<3; i++) {
             let t = new Date().getTime()
-            let h5st = await this.signWaap('d2f64', {
-                appid: 'siteppM',
-                functionId: 'siteppM_skuOnceApply',
-                t,
-                body: {sid: '', type: '25', forcebot: ''}
-            })
-            s = await this.curl({
-                    'url': `https://api.m.jd.com/api?appid=siteppM&functionId=siteppM_skuOnceApply&forcebot=&t=${t}`,
-                    "form": `body=${this.dumps({
+            let s = await this.algo.curl({
+                    'url': `https://api.m.jd.com/api`,
+                    "form": `appid=siteppM&functionId=siteppM_skuOnceApply&forcebot=&t=${t}&body=${this.dumps({
                         sid: "",
                         type: "25",
                         forcebot: "",
-                    })}&h5st=${encodeURIComponent(h5st)}`,
+                    })}`,
                     cookie,
                     ua: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:100.0) Gecko/20100101 Firefox/100.0',
                     "referer": "https://msitepp-fm.jd.com/",
                 }
             )
+            console.log(s)
             if (s.flag == true) {
                 break
             }
@@ -74,67 +67,6 @@ class Main extends Template {
             text = "本次无保价订单"
         }
         console.log(p.user, text)
-    }
-
-    async extra() {
-        this.dom.window.close()
-    }
-
-    async waap() {
-        let {
-            JSDOM
-        } = this.modules.jsdom;
-        let resourceLoader = new this.modules.jsdom.ResourceLoader({
-            userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:100.0) Gecko/20100101 Firefox/100.0',
-            referrer: "https://msitepp-fm.jd.com/rest/priceprophone/priceProPhoneMenu",
-        });
-        let options = {
-            url: "http://127.0.0.1/h5st/h5st.html",
-            runScripts: "dangerously",
-            resources: resourceLoader,
-        }
-        this.dom = new JSDOM(`<body>
-         <script type="text/javascript" src="https://static.360buyimg.com/siteppStatic/script/mescroll/map.js"></script>
-  <script type="text/javascript" src="https://storage.360buyimg.com/webcontainer/js_security_v3_0.1.0.js"></script>
-  <script type="text/javascript" src="https://static.360buyimg.com/siteppStatic/script/utils.js"></script>
-  <script type="text/javascript" src="https://js-nocaptcha.jd.com/statics/js/main.min.js"></script>
-</body>`, options);
-        await this.wait(3000)
-        this.signWaap = this.dom.window.signWaap
-    }
-
-    async jstoken() {
-        let {
-            JSDOM
-        } = this.modules.jsdom;
-        let resourceLoader = new this.modules.jsdom.ResourceLoader({
-            userAgent: `Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:91.0) Gecko/20100${this.rand(100, 999)} Firefox/91.0`,
-            referrer: "https://msitepp-fm.jd.com/rest/priceprophone/priceProPhoneMenu",
-        });
-        let virtualConsole = new this.modules.jsdom.VirtualConsole();
-        var options = {
-            url: "https://msitepp-fm.jd.com/rest/priceprophone/priceProPhoneMenu",
-            referrer: "https://msitepp-fm.jd.com/rest/priceprophone/priceProPhoneMenu",
-            userAgent: `Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:91.0) Gecko/20100${this.rand(100, 999)} Firefox/91.0`,
-            runScripts: "dangerously",
-            resources: resourceLoader,
-            //  cookieJar,
-            includeNodeLocations: true,
-            storageQuota: 10000000,
-            pretendToBeVisual: true,
-            virtualConsole
-        };
-        this.dom = new JSDOM(`<body><script src="https://js-nocaptcha.jd.com/statics/js/main.min.js"></script></body>`, options);
-        await this.wait(5000)
-        try {
-            let feSt = 's'
-            let jab = new this.dom.window.JAB({
-                bizId: 'jdjiabao',
-                initCaptcha: false
-            })
-            this.token = jab.getToken()
-        } catch (e) {
-        }
     }
 }
 
