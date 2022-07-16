c1813e8e6d91c9bb78dfd5c4fcc4f98eb80dfbb0
air <92066902+qitoqito@users.noreply.github.com>
2022-07-16 09:01:51 +0800

    Update jd_task_united.js

--- parse/jd/jd_task_united.js
+++ parse/jd/jd_task_united.js
@@ -4,7 +4,7 @@ class Main extends Template {
     constructor() {
         super()
         this.title = "京东大牌集合任务"
-        // this.cron = "12 0,13 * * *"
+        this.cron = "6 6 6 6 6"
         this.task = 'local'
         this.import = ['jdUrl']
         this.verify = 1
@@ -56,118 +56,125 @@ class Main extends Template {
                 console.log(`用户过期或者异常`)
                 return
             }
-            let load = await this.curl({
-                    'url': `https://jinggengjcq-isv.isvjcloud.com/dm/front/openCardNew/activity_load?mix_nick=`,
-                    'json': {
-                        "jsonRpc": "2.0",
-                        "params": {
-                            "commonParameter": {
-                                "appkey": "51B59BB805903DA4CE513D29EC448375",
-                                "m": "POST",
-                                "timestamp": this.timestamp,
-                                // "userId": "10299181"
-                            },
-                            "admJson": {
-                                "actId": p.inviter.actId,
-                                // "userId": "10299181",
-                                "jdToken": isvObfuscator.token,
-                                "source": "01",
-                                "method": "/openCardNew/activity_load",
-                                "buyerNick": ""
-                            }
-                        }
-                    },
-                }
-            )
-            if (this.haskey(load, 'errorMessage', '获取京东用户信息失败~')) {
-                console.log('获取京东用户信息失败~')
-                return
-            }
-            let buyerNick = this.haskey(load, 'data.data.buyerNick')
-            let userId = (this.haskey(load, 'data.data.missionCustomer.userId') || 10299171).toString()
-            for (let nn of Array(3)) {
-                var state = await this.curl({
-                        'url': `https://jinggengjcq-isv.isvjcloud.com/dm/front/openCardNew/mission/complete/state?mix_nick=${buyerNick}`,
+            for (let i of Array(5)) {
+                var load = await this.curl({
+                        'url': `https://jinggengjcq-isv.isvjcloud.com/dm/front/openCardNew/activity_load?mix_nick=`,
                         'json': {
                             "jsonRpc": "2.0",
                             "params": {
                                 "commonParameter": {
                                     "appkey": "51B59BB805903DA4CE513D29EC448375",
                                     "m": "POST",
-                                    "timestamp": new Date().getTime(),
-                                    userId
+                                    "timestamp": this.timestamp,
+                                    // "userId": "10299181"
                                 },
                                 "admJson": {
                                     "actId": p.inviter.actId,
-                                    userId,
-                                    "method": "/openCardNew/mission/complete/state",
-                                    buyerNick
+                                    // "userId": "10299181",
+                                    "jdToken": isvObfuscator.token,
+                                    "source": "01",
+                                    "method": "/openCardNew/activity_load",
+                                    "buyerNick": ""
                                 }
                             }
-                        }
+                        },
                     }
                 )
-                if (this.haskey(state, 'data.data')) {
+                if (this.haskey(load, 'data.data.buyerNick')) {
                     break
                 }
             }
-            this.assert(this.haskey(state, 'data.data'), '没有获取到数据')
-            for (let i of this.haskey(state, 'data.data')) {
-                if (!i.isComplete) {
-                    let k = 0
-                    if (['viewShop', 'viewGoods', 'uniteAddCart', 'uniteCollectShop'].includes(i.type)) {
-                        console.log('正在运行:', i.missionName)
-                        let json = {
-                            "jsonRpc": "2.0",
-                            "params": {
-                                "commonParameter": {
-                                    "appkey": "51B59BB805903DA4CE513D29EC448375",
-                                    "m": "POST",
-                                    "timestamp": new Date().getTime(),
-                                    userId
-                                },
-                                "admJson": {
-                                    "actId": p.inviter.actId,
-                                    "missionType": i.type,
-                                    "method": "/openCardNew/complete/mission",
-                                    userId,
-                                    buyerNick
+            if (this.haskey(load, 'errorMessage', '获取京东用户信息失败~')) {
+                console.log('获取京东用户信息失败~')
+                return
+            }
+            let buyerNick = this.haskey(load, 'data.data.buyerNick')
+            let userId = (this.haskey(load, 'data.data.missionCustomer.userId') || 10299171).toString()
+            for (let zz of Array(2)) {
+                for (let nn of Array(3)) {
+                    var state = await this.curl({
+                            'url': `https://jinggengjcq-isv.isvjcloud.com/dm/front/openCardNew/mission/complete/state?mix_nick=${buyerNick}`,
+                            'json': {
+                                "jsonRpc": "2.0",
+                                "params": {
+                                    "commonParameter": {
+                                        "appkey": "51B59BB805903DA4CE513D29EC448375",
+                                        "m": "POST",
+                                        "timestamp": new Date().getTime(),
+                                        userId
+                                    },
+                                    "admJson": {
+                                        "actId": p.inviter.actId,
+                                        userId,
+                                        "method": "/openCardNew/mission/complete/state",
+                                        buyerNick
+                                    }
                                 }
                             }
                         }
-                        for (let j = 0; j<(i.dayTop - (i.hasGotNum || 0)); j++) {
-                            let s = await this.curl({
-                                    'url': `https://jinggengjcq-isv.isvjcloud.com/dm/front/openCardNew/complete/mission?mix_nick=${buyerNick}`,
-                                    json
-                                }
-                            )
-                            let remark = this.haskey(s, 'data.data.remark') || ''
-                            let num = this.match(/(\d+)个京豆/, remark)
-                            if (num) {
-                                k = 0
-                                gifts += parseInt(num)
-                            }
-                            else {
-                                k++
-                            }
-                            console.log(remark || '什么也没有啊')
-                            if (k == 3) {
-                                console.log(`检测到没有豆子奖励,退出浏览`)
-                                break
-                            }
-                            await this.wait(this.rand(1000, 2000))
-                        }
+                    )
+                    if (this.haskey(state, 'data.data')) {
+                        break
                     }
                 }
-                else {
-                    console.log(`任务完成:`, i.missionName)
+                this.assert(this.haskey(state, 'data.data'), '没有获取到数据')
+                for (let i of this.haskey(state, 'data.data')) {
+                    if (!i.isComplete) {
+                        let k = 0
+                        if (['viewShop', 'viewGoods', 'uniteAddCart', 'uniteCollectShop'].includes(i.type)) {
+                            console.log('正在运行:', i.missionName)
+                            let json = {
+                                "jsonRpc": "2.0",
+                                "params": {
+                                    "commonParameter": {
+                                        "appkey": "51B59BB805903DA4CE513D29EC448375",
+                                        "m": "POST",
+                                        "timestamp": new Date().getTime(),
+                                        userId
+                                    },
+                                    "admJson": {
+                                        "actId": p.inviter.actId,
+                                        "missionType": i.type,
+                                        "method": "/openCardNew/complete/mission",
+                                        userId,
+                                        buyerNick
+                                    }
+                                }
+                            }
+                            for (let j = 0; j<(i.dayTop - (i.hasGotNum || 0)); j++) {
+                                let s = await this.curl({
+                                        'url': `https://jinggengjcq-isv.isvjcloud.com/dm/front/openCardNew/complete/mission?mix_nick=${buyerNick}`,
+                                        json,
+                                    }
+                                )
+                                let remark = this.haskey(s, 'data.data.remark') || ''
+                                let num = this.match(/(\d+)个京豆/, remark)
+                                if (num) {
+                                    k = 0
+                                    gifts += parseInt(num)
+                                }
+                                else {
+                                    k++
+                                }
+                                console.log(remark || '什么也没有啊')
+                                if (k == 3) {
+                                    console.log(`检测到没有豆子奖励,退出浏览`)
+                                    break
+                                }
+                                await this.wait(this.rand(1000, 2000))
+                            }
+                        }
+                    }
+                    else {
+                        console.log(`任务完成:`, i.missionName)
+                    }
                 }
-            }
-            if (gifts) {
-                this.print(`获得京豆: ${gifts}`, p.user)
-            }
-            else {
-                console.log(`什么也没有`)
+                if (gifts) {
+                    this.print(`获得京豆: ${gifts}`, p.user)
+                }
+                else {
+                    console.log(`什么也没有`)
+                }
             }
             let skuList = p.inviter.skuList
             if (skuList.length>0) {
