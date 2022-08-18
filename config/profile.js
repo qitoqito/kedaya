1cf61a38cffa9c022df62014aa1017ff0be85e31
air <92066902+qitoqito@users.noreply.github.com>
2022-08-18 19:37:51 +0800

    Update profile.js

--- config/profile.js
+++ config/profile.js
@@ -5,5 +5,5 @@ module.exports = {
         'kejiwanjia': [/pin=([^;]+)/, /username=([^;]+)/],
         'baidu': /pin=([^;]+)/,
     },
-    communal: ['options', 'curl', 'jsonParse', 'uuid', 'response', 'location', 'dumps', 'loads', 'match', 'matchAll', 'md5', 'unique', 'rand', 'random', 'compact', 'column', 'wait', 'query', 'sha1', 'md5', 'sha256', 'hmacsha256', 'query', 'type', 'parseIni', 'redisCli']
+    communal: ['options', 'curl', 'jsonParse', 'uuid', 'response', 'location', 'dumps', 'loads', 'match', 'matchAll', 'md5', 'unique', 'rand', 'random', 'compact', 'column', 'wait', 'query', 'sha1', 'md5', 'sha256', 'hmacsha256', 'query', 'type', 'parseIni', 'redisCli', 'fileCache']
 }
