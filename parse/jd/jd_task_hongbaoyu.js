39629836dd508a40eb6234edab01a6f80d2aa74d
air <92066902+qitoqito@users.noreply.github.com>
2022-09-01 20:22:18 +0800

    Update jd_task_hongbaoyu.js

--- parse/jd/jd_task_hongbaoyu.js
+++ parse/jd/jd_task_hongbaoyu.js
@@ -4,16 +4,23 @@ class Main extends Template {
     constructor() {
         super()
         this.title = "京东超市红包雨"
-        this.cron = "6 6 6 6 6"
+        // this.cron = "2 0,20 * * *"
         this.help = 'main'
         this.task = 'local'
         this.verify = 1
+        this.import = ['jdAlgo']
     }
 
     async prepare() {
-        let projectId = this.profile.custom || '40884421925727823'
+        this.algo = new this.modules.jdAlgo()
+        this.algo.set({
+            'appId': '16073',
+            'type': 'web',
+            "version": "3.0",
+        })
+        let projectId = this.profile.custom || '77983495650348761'
         for (let cookie of this.cookies.help) {
-            let s = await this.curl({
+            let s = await this.algo.curl({
                     'url': `https://api.m.jd.com/api?appid=hongbaoyu&functionId=redRainInitProjectScene&body={"projectId":"${projectId}"}`,
                     cookie
                 }
@@ -31,12 +38,13 @@ class Main extends Template {
     async main(p) {
         let cookie = p.cookie;
         for (let i = 0; i<3; i++) {
-            let s = await this.curl({
+            let s = await this.algo.curl({
                     'url': `https://api.m.jd.com/api?appid=hongbaoyu&functionId=redRainStartLottery&body={"projectId":"${p.inviter.projectId}","activityId":"${p.inviter.activityId}"}`,
                     // 'form':``,
                     cookie
                 }
             )
+            console.log(s)
             if (this.haskey(s, 'data.name')) {
                 this.print(`获得: ${s.data.name}`, p.user)
             }
