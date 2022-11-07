04039a149b5c6cc873cf5afdb0c6c98cc849daf5
air <92066902+qitoqito@users.noreply.github.com>
2022-11-07 12:53:11 +0800

    Update jd_task_beanList.js

--- parse/jd/jd_task_beanList.js
+++ parse/jd/jd_task_beanList.js
@@ -16,6 +16,51 @@ class Main extends Template {
         let x = this.getDate(this.timestamp, 0, '-')
         let r = new RegExp(`${x}`)
         let xs = []
+        let s = await this.curl({
+                'url': `https://api.m.jd.com/?appid=jd-cphdeveloper-m&functionId=myBean&body={"tenantCode":"jgminise","bizModelCode":6,"bizModeClientType":"WxMiniProgram","externalLoginType":1}&sceneval=2&g_login_type=1&callback=draw_activetemporary&g_tk=610895985&g_ty=ls`,
+                // 'form':``,
+                cookie
+            }
+        )
+        for (let k of this.haskey(s, 'list')) {
+            if (k.createDate.includes(x)) {
+                xs.push(k)
+            }
+        }
+        let z = [], f = []
+        let d = {}
+        for (let i of xs) {
+            d[i.visibleInfo] = d[i.visibleInfo] || {
+                eventMassage: i.visibleInfo.replace(/参加|店铺活动|-奖励|\[|\]/g, ''),
+                amount: 0
+            }
+            d[i.visibleInfo].amount += parseInt(i.amount)
+        }
+        let dict = Object.values(d).sort(function(a, b) {
+            return b.amount - a.amount
+        })
+        let echo = [`🐹  今日总共收入: ${this.sum(this.column(dict, 'amount').filter(d => d>0)) || 0}  支出: ${this.sum(this.column(dict, 'amount').filter(d => d<0)) || 0}`]
+        for (let i of dict) {
+            if (parseInt(i.amount)<0) {
+                echo.push(`🐶  [${i.amount}] ${i.eventMassage}`)
+            }
+            else {
+                echo.push(`🦁  [${i.amount}] ${i.eventMassage}`)
+            }
+        }
+        if (this.haskey(s,'willExpireNum')) {
+            echo.push(`🙊  [${s.willExpireNum}] 即将过期`)
+        }
+        console.log(echo.join("\n"))
+        this.notices(echo.join("\n"), p.user, 5)
+
+    }
+
+    async main123(p) {
+        let cookie = p.cookie;
+        let x = this.getDate(this.timestamp, 0, '-')
+        let r = new RegExp(`${x}`)
+        let xs = []
         for (let i = 1; i<20; i++) {
             let params = {
                 "url": `https://api.m.jd.com/client.action?functionId=getJingBeanBalanceDetail`,
@@ -36,7 +81,7 @@ class Main extends Template {
         let d = {}
         for (let i of xs) {
             d[i.eventMassage] = d[i.eventMassage] || {
-                eventMassage: i.eventMassage.replace(/参加|店铺活动|-奖励|\[|\]/g,''),
+                eventMassage: i.eventMassage.replace(/参加|店铺活动|-奖励|\[|\]/g, ''),
                 amount: 0
             }
             d[i.eventMassage].amount += parseInt(i.amount)
