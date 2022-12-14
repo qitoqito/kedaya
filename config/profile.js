d2d2df71043f6e7cb1b755bbc4e3705608f9233b
air <92066902+qitoqito@users.noreply.github.com>
2022-12-14 16:00:56 +0800

    Update profile.js

--- config/profile.js
+++ config/profile.js
@@ -1,5 +1,5 @@
 module.exports = {
-    keywords: ['model', 'task', 'thread', 'help', 'exit', 'limit', 'except', 'reward', 'loop', 'interval', 'cron', 'expand', 'custom', 'turn', 'endTime', 'send', 'work', 'msgNum', 'msgTotal', 'msgExcept', 'msgWork', 'count', 'proxy', 'timeout', 'skip', 'aid', 'hideCode', 'timer'],
+    keywords: ['model', 'task', 'thread', 'help', 'exit', 'limit', 'except', 'reward', 'loop', 'interval', 'cron', 'expand', 'custom', 'turn', 'endTime', 'send', 'work', 'msgNum', 'msgTotal', 'msgExcept', 'msgWork', 'count', 'proxy', 'timeout', 'skip', 'aid', 'hideCode', 'timer','delay'],
     userRegular: {
         'jd': /pin=([^;]+)/,
         'kejiwanjia': [/pin=([^;]+)/, /username=([^;]+)/],
