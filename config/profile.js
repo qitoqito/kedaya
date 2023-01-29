3714e12adc8e5094fe2dece907ada7dacebdeb63
air <92066902+qitoqito@users.noreply.github.com>
2023-01-29 21:17:56 +0800

    新增proxy代理切换
    在jd.ini同目录下新建proxy.ini
    
    [jd]
    http://ip:port
    http://ip2:port2
    
    然后在脚本节点添加
    urlProxy=jd

--- config/profile.js
+++ config/profile.js
@@ -1,11 +1,11 @@
 module.exports = {
-    keywords: ['model', 'task', 'thread', 'help', 'exit', 'limit', 'except', 'reward', 'loop', 'interval', 'cron', 'expand', 'custom', 'turn', 'endTime', 'send', 'work', 'msgNum', 'msgTotal', 'msgExcept', 'msgWork', 'count', 'proxy', 'timeout', 'skip', 'aid', 'hideCode', 'timer', 'delay'],
+    keywords: ['model', 'task', 'thread', 'help', 'exit', 'limit', 'except', 'reward', 'loop', 'interval', 'cron', 'expand', 'custom', 'turn', 'endTime', 'send', 'work', 'msgNum', 'msgTotal', 'msgExcept', 'msgWork', 'count', 'proxy', 'timeout', 'skip', 'aid', 'hideCode', 'timer', 'delay', 'urlProxy'],
     userRegular: {
         'jd': /pin=([^;]+)/,
         'kejiwanjia': [/pin=([^;]+)/, /username=([^;]+)/],
         'baidu': /pin=([^;]+)/,
     },
-    communal: ['options', 'curl', 'jsonParse', 'uuid', 'response', 'location', 'dumps', 'loads', 'match', 'matchAll', 'md5', 'unique', 'rand', 'random', 'compact', 'column', 'wait', 'query', 'sha1', 'md5', 'sha256', 'hmacsha256', 'query', 'haskey', 'type', 'parseIni', 'redisCli', 'fileCache', 'userAgents'],
+    communal: ['options', 'curl', 'jsonParse', 'uuid', 'response', 'location', 'dumps', 'loads', 'match', 'matchAll', 'md5', 'unique', 'rand', 'random', 'compact', 'column', 'wait', 'query', 'sha1', 'md5', 'sha256', 'hmacsha256', 'query', 'haskey', 'type', 'parseIni', 'redisCli', 'fileCache', 'userAgents', 'userName', 'userPin', 'userDict', 'userRegular', 'urlProxy', 'proxyRow', 'proxyList', 'delay'],
     timerRegular: {
         jd: {
             wait: 120,
