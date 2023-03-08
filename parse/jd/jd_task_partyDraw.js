ff14a11572b071850ff531def32b911570b0bc7a
air <92066902+qitoqito@users.noreply.github.com>
2023-03-08 21:26:07 +0800

    Update jd_task_partyDraw.js

--- parse/jd/jd_task_partyDraw.js
+++ parse/jd/jd_task_partyDraw.js
@@ -18,7 +18,7 @@ class Main extends Template {
         let cookie = p.cookie;
         let gifts = []
         for (let i of Array(50)) {
-            let s = await this.curl({
+            let s = await this.algo.curl({
                     'url': `https://api.m.jd.com/?functionId=wheelsLottery&body={"linkId":"7m_tf2OArtOndOSDV7IWeQ"}&t=1678281038150&appid=activities_platform&client=ios&clientVersion=11.6.3&build=168563&screen=390*844&networkType=wifi&d_brand=iPhone&d_model=iPhone13,3&lang=zh_CN&osVersion=15.1.1&partner=&eid=eidId7f9812189s4Ywiz164KTQqoeSyoW1uZwmMItV216n8pCJ26eJPEqZb5n8VkyLjW71hRQ6fhLku8USG3jg%2BHtZ7ecv%2BJ2CWEYpUd99P1GvH7bppT`,
                     // 'form':``,
                     cookie,
