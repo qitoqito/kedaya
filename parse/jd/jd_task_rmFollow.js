7b5c96cdacc30cb2e3c8242146bfcc73613bb942
air <92066902+qitoqito@users.noreply.github.com>
2023-04-18 18:10:33 +0800

    Update jd_task_rmFollow.js

--- parse/jd/jd_task_rmFollow.js
+++ parse/jd/jd_task_rmFollow.js
@@ -4,46 +4,68 @@ class Main extends Template {
     constructor() {
         super()
         this.title = "京东取关店铺"
-        this.cron = "33 23 * * *"
-        this.task = 'local'
         this.cron = "6 6 6 6 6"
+        this.task = 'local'
     }
 
     async main(p) {
-        let cookie = p.cookie
-        var count = this.profile.count || 100
-        let bit = '16' + this.matchAll(/(\d)/g, new Buffer.from(`${p.user},${new Date().getDay()}`).toString('hex') + '8095').join('').slice(0, 6)
-        let ua = this.profile.userAgent || `JD4iPhone/${bit}%20(iPhone;%20iOS;%20Scale/3.00)`
-        let url = `https://wq.jd.com/fav/shop/QueryShopFavList?cp=1&pageSize=20&lastlogintime=${this.timestamp}&_=1629620296971&g_login_type=0&callback=jsonpCBKA&g_tk=1994796340&g_ty=ls&sceneval=2&g_login_type=1`
-        let array = []
-        let nn = 0
-        let a = 0
-        for (let i = 0, n = Math.ceil(count / 20); i<n; i++) {
-            let lists = await this.curl({url, cookie: p.cookie})
-            if (lists.data) {
-                // console.log(lists.data)
-                let ss = await this.curl({
-                        'url': `https://api.m.jd.com/client.action?functionId=followShop`,
-                        'form': `functionId=followShop&body={"follow":"false","shopId":"${this.column(lists.data, 'shopId').join(",")}","award":"true","sourceRpc":"shop_app_home_follow"}&osVersion=13.7&appid=wh5&clientVersion=9.2.0&loginType=2&loginWQBiz=interact`,
-                        cookie, ua
+        let cookie = p.cookie;
+        let count = parseInt(this.profile.count || this.dict.count || 20)
+        let page = 1
+        for (let i = 0; i<count / 20; i++) {
+            let s = await this.curl({
+                    'url': `https://wq.jd.com/fav/shop/QueryShopFavList?cp=${page}&pageSize=20&lastlogintime=1681810631&_=1681810636733&g_login_type=0&appCode=msd95910c4&callback=jsonpCBKA&g_tk=646642342&g_ty=ls`,
+                    cookie
+                }
+            )
+            if (this.haskey(s, 'data')) {
+                // console.log(s.data)
+                console.log("当前店铺收藏数:", s.totalNum)
+                if (s.totalNum>0) {
+                    page = Math.ceil(s.totalNum / 20)>1 ? this.rand(1, Math.ceil(s.totalNum / 20)) : 1
+                    // shopName
+                    if (this.profile.whiteList || this.dict.whiteList) {
+                        let whiteList = (this.profile.whiteList || this.dict.whiteList).split(",").join('|')
+                        var data = s.data.filter(d => !d.shopName.match(whiteList))
                     }
-                )
-                console.log(ss.msg || ss)
-                if (ss.msg) {
-                    a += lists.data.length
+                    else if (this.profile.blackList || this.dict.blackList) {
+                        let blackList = (this.profile.blackList || this.dict.blackList).blackList.split(",").join('|')
+                        var data = s.data.filter(d => d.shopName.match(blackList))
+                    }
+                    else {
+                        var data = s.data
+                    }
+                    // console.log(s.data)
+                    // console.log(this.column(s.data, 'shopName').join("|"))
+                    if (data.length>0) {
+                        console.log(`正在删除:`, this.column(data, 'shopName').join("|"))
+                        // let rm = await this.curl({
+                        //         'url': `https://api.m.jd.com/api?functionId=batchCancelFavorite&body={"skus":"${this.column(data, 'shopId').join(",")}"}&appid=follow_for_concert&client=pc`,
+                        //         cookie
+                        //     }
+                        // )
+                        // console.log(rm.resultMsg)
+                        let rm = await this.curl({
+                                'url': `https://wq.jd.com/fav/shop/batchunfollow?shopId=${this.column(data, 'shopId').join(",")}&_=1681810861733&g_login_type=0&appCode=msd95910c4&callback=jsonpCBKE&g_tk=646642342&g_ty=ls`,
+                                // 'form':``,
+                                cookie
+                            }
+                        )
+                        console.log(rm)
+                    }
+                    else {
+                        break
+                    }
                 }
                 else {
-                    console.log("失败了")
+                    console.log("没有获取关注数据")
                     break
                 }
             }
             else {
-                console.log("未获取到店铺关注信息")
+                console.log("可能黑ip了,没有获取到数据")
                 break
             }
-        }
-        if (a>0) {
-            this.notices(`取关店铺数: ${a}`, p.user, 30)
         }
     }
 }
