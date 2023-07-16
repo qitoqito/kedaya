8d8a37a1e654bcd04708492b7ee129b2b630c0e9
air <92066902+qitoqito@users.noreply.github.com>
2023-07-16 13:43:44 +0800

    inviteFission

--- parse/jd/js_task_inviteFission.js
+++ parse/jd/js_task_inviteFission.js
@@ -3,7 +3,7 @@ const Template = require('../../template');
 class Main extends Template {
     constructor() {
         super()
-        this.title = "京东抽现金赢大礼"
+        this.title = "京东特价抽现金赢大礼"
         this.cron = "6 6 6 6 6"
         this.help = 'main'
         this.task = 'local'
@@ -22,7 +22,7 @@ class Main extends Template {
     }
 
     async prepare() {
-        this.linkId = this.custom || '3orGfh1YkwNLksxOcN8zWQ'
+        this.linkId = this.custom || 'Wvzc_VpNTlSkiQdHT8r7QA'
         this.algo = new this.modules.jdAlgo()
         console.log("获取助力码中...")
         for (let cookie of this.cookies.help) {
