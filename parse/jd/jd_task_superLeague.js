d604cbee8c93426e13fd9916b18763fca6d2e548
可达鸭 <ii@iippcc.com>
2024-02-07 22:41:44 +0800

    Update jd_task_superLeague.js

--- parse/jd/jd_task_superLeague.js
+++ parse/jd/jd_task_superLeague.js
@@ -3,7 +3,7 @@ const Template = require('../../template');
 class Main extends Template {
     constructor() {
         super()
-        this.title = "京东11.11魔法开启"
+        this.title = "京东新春吉市"
         this.cron = `${this.rand(0, 59)} ${this.rand(0, 9)},${this.rand(14, 21)} * * *`
         this.task = 'local'
         this.import = ['jdAlgo']
@@ -15,7 +15,8 @@ class Main extends Template {
             version: '4.1',
             type: "main"
         })
-        this.linkId = this.profile.custom || 'toZwuImFnkGzWBE82w3RLA'
+        this.linkId = this.profile.linkId || 'mS2PkOqrtmfneDMmhJ3dbQ'
+        this.clientVersion = '11.6.3'
     }
 
     async main(p) {
@@ -26,7 +27,6 @@ class Main extends Template {
             algo: {'appId': 'b7d17'},
             cookie
         })
-        // console.log(home)
         if (this.haskey(home, 'data.notLogin')) {
             console.log("账号过期...")
             return
@@ -50,27 +50,71 @@ class Main extends Template {
             else {
                 for (let j of Array(i.taskLimitTimes - i.taskDoTimes)) {
                     console.log("正在运行:", i.taskShowTitle)
-                    let doTask = await this.wget({
-                        fn: 'apsDoTask',
-                        body: {
-                            "linkId": this.linkId,
-                            "taskType": i.taskType,
-                            "taskId": i.id,
-                            "channel": 4,
-                            "checkVersion": true,
-                            "cityId": "",
-                            "provinceId": "",
-                            "countyId": "",
-                            "itemId": encodeURIComponent(i.taskSourceUrl)
-                        },
-                        algo: {'appId': '54ed7'},
-                        cookie
-                    })
-                    if (this.haskey(doTask, 'success')) {
-                        console.log("任务完成")
-                    }
-                    else {
-                        console.log("任务失败:", this.haskey(doTask, 'errMsg') || doTask)
+                    switch (i.taskType) {
+                        case 'SHARE_INVITE':
+                            break
+                        case 'BROWSE_CHANNEL':
+                        case  'BROWSE_PRODUCT' :
+                            let detail = await this.wget({
+                                fn: 'apTaskDetail',
+                                body: {
+                                    "linkId": this.linkId,
+                                    "taskType": i.taskType,
+                                    "taskId": i.id,
+                                    "channel": 4,
+                                    "checkVersion": true,
+                                    "cityId": "",
+                                    "provinceId": "",
+                                    "countyId": "",
+                                },
+                                algo: {'appId': '54ed7'},
+                                cookie
+                            })
+                            if (this.haskey(detail, 'data.taskItemList')) {
+                                let doTask = await this.wget({
+                                    fn: 'apsDoTask',
+                                    body: {
+                                        "linkId": this.linkId,
+                                        "taskType": i.taskType,
+                                        "taskId": i.id,
+                                        "channel": 4,
+                                        "checkVersion": true,
+                                        "cityId": "",
+                                        "provinceId": "",
+                                        "countyId": "",
+                                        "itemId": encodeURIComponent((detail.data.taskItemList[j] || detail.data.taskItemList[0]).itemId)
+                                    },
+                                    algo: {'appId': '54ed7'},
+                                    cookie
+                                })
+                                console.log(this.haskey(doTask, 'success'))
+                            }
+                            break
+                        default:
+                            let doTask = await this.wget({
+                                fn: 'apsDoTask',
+                                body: {
+                                    "linkId": this.linkId,
+                                    "taskType": i.taskType,
+                                    "taskId": i.id,
+                                    "channel": 4,
+                                    "checkVersion": true,
+                                    "cityId": "",
+                                    "provinceId": "",
+                                    "countyId": "",
+                                    "itemId": encodeURIComponent(i.taskSourceUrl)
+                                },
+                                algo: {'appId': '54ed7'},
+                                cookie
+                            })
+                            console.log(i.taskType)
+                            if (this.haskey(doTask, 'success')) {
+                                console.log("任务完成")
+                            }
+                            else {
+                                console.log("任务失败:", this.haskey(doTask, 'errMsg') || doTask)
+                            }
+                            break
                     }
                 }
             }
@@ -79,7 +123,7 @@ class Main extends Template {
             try {
                 let lottery = await this.wget({
                     fn: 'superLeagueLottery',
-                    body: {"linkId": "toZwuImFnkGzWBE82w3RLA"},
+                    body: {"linkId": "mS2PkOqrtmfneDMmhJ3dbQ"},
                     algo: {'appId': '60dc4'},
                     cookie
                 })
@@ -99,6 +143,9 @@ class Main extends Template {
                     }
                     else if (prizeType == 2) {
                         this.print(`获得: 红包   ${data.amount}`, p.user)
+                    }
+                    else if (prizeType == 22) {
+                        this.print(`获得: 超市卡   ${data.amount}`, p.user)
                     }
                     else if (prizeType == 0) {
                         console.log('没抽到奖品')
@@ -121,7 +168,7 @@ class Main extends Template {
     async wget(p) {
         return await this.algo.curl({
                 'url': `https://api.m.jd.com/client.action`,
-                'form': `functionId=${p.fn}&body=${typeof (p.body) == 'object' ? this.dumps(p.body) : p.body}&t=1698649904893&appid=activities_platform&client=ios&clientVersion=12.1.6&build=168909&screen=390*844&networkType=wifi&d_brand=iPhone&d_model=iPhone13,3&lang=zh_CN&osVersion=15.1.1&partner=-1&cthr=1`,
+                'form': `functionId=${p.fn}&body=${typeof (p.body) == 'object' ? this.dumps(p.body) : p.body}&t=1698649904893&appid=activities_platform&client=ios&clientVersion=${this.clientVersion}&build=168909&screen=390*844&networkType=wifi&d_brand=iPhone&d_model=iPhone13,3&lang=zh_CN&osVersion=15.1.1&partner=-1&cthr=1`,
                 cookie: p.cookie,
                 algo: p.algo || {},
             }
