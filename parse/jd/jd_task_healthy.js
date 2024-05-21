0cc7e9782a0dd302a4c6f7351d04fe2e3f085874
可达鸭 <ii@iippcc.com>
2024-05-21 21:19:47 +0800

    Update jd_task_healthy.js

--- parse/jd/jd_task_healthy.js
+++ parse/jd/jd_task_healthy.js
@@ -18,7 +18,7 @@ class Main extends Template {
         let cookie = p.cookie;
         var bean = 0
         let gift = function(a) {
-            for (let i of this.haskey(a, 'data.result.prizeInfovos')) {
+            for (let i of this.haskey(a, 'result.result.prizeInfovos')) {
                 if (i.prizeType == 2) {
                     bean += parseInt(i.awardId)
                     console.log(`获得京豆:`, i.awardId)
@@ -28,10 +28,16 @@ class Main extends Template {
                 }
             }
         }
+        let headers = {
+            referer: 'https://jd.com',
+            'j-e-c': `%7B%22ciphertype%22:5,%22cipher%22:%7B%22pin%22:%22awHpZXZ1JQrEHW9uHvvB%22%7D,%22ts%22:${parseInt(new Date().getTime()/1000)},%22hdid%22:%22JM9F1ywUPwflvMIpYPok0tt5k9kW4ArJEU3lfLhxBqw=%22,%22version%22:%221.0.3%22,%22appname%22:%22com.360buy.jdmobile%22,%22ridx%22:-1%7D`,
+            'jdgs': `{"b1":"${this.uuid(8)}-${this.uuid(4)}-${this.uuid(4)}-${this.uuid(4)}-${this.uuid(12)}","b2":"2.2.4_1","b3":"2.1","b4":"","b5":"${this.sha1(p.cookie)}","b7": ${new Date().getTime().toString()},"b6":"${this.sha1(p.user)}"}`
+        }
         let h = await this.sign.jdCurl({
                 'url': `https://api.m.jd.com/client.action`,
                 'form': `body={"osName":"buyMedicine","channel":"jdapp","methodName":"getKitTask2727","groupCode":"openkits","m_patch_appKey":"231282000001","imei":"OOLPCJFL","wrapResult":true,"appId":"2d0050d8877d4065b6227fb9fdcc01bb","version":12,"location":{"district":"1234","town":"56789","province":"16","city":"1234"}}&build=169227&client=apple&clientVersion=12.6.2&functionId=jdh_laputa_handleSoaRequest_reinforce`,
-                cookie
+                cookie,
+                headers
             }
         )
         let encodeId = this.haskey(h, 'data.result.encodeId')
@@ -42,12 +48,12 @@ class Main extends Template {
         let res = h.data.result
         if (res.status == 1) {
             let sign = await this.sign.jdCurl({
-                    'url': `https://api.m.jd.com/client.action?functionId=jdh_laputa_handleSoaRequest_reinforce`,
-                    'form': `body={"osName":"buyMedicine","infoId":"jdhHome_task","methodName":"doTask2745","encodeId":"${encodeId}","appKey":"231282000001","channel":"jdapp","wrapResult":true,"appId":"2d0050d8877d4065b6227fb9fdcc01bb","version":12,"imei":"OOLPCJFL","location":{"district":"1244","town":"56789","province":"16","city":"1234"}}&build=169227&client=apple&clientVersion=12.6.2&functionId=jdh_laputa_handleSoaRequest_reinforce`,
-                    cookie
+                    'url': `https://api.m.jd.com/client.action`,
+                    'form': `body={"osName":"buyMedicine","infoId":"jdhHome_task","methodName":"doTask2745","encodeId":"${encodeId}","appKey":"231282000001","channel":"jdapp","wrapResult":true,"appId":"2d0050d8877d4065b6227fb9fdcc01bb","version":12,"imei":"OOLPCJFL","location":{"district":"1244","town":"56789","province":"16","city":"1234"}}&build=169227&client=apple&clientVersion=12.6.2&functionId=jdh_msoa_doTaskGw`,
+                    cookie, headers
                 }
             )
-            if (this.haskey(sign, 'data.result.signType')) {
+            if (this.haskey(sign, 'result.result.signType')) {
                 gift.call(this, sign)
             }
             else {
@@ -58,7 +64,8 @@ class Main extends Template {
         let list = await this.sign.jdCurl({
                 'url': `https://api.m.jd.com/client.action?functionId=jdh_laputa_handleSoaRequest_reinforce`,
                 'form': `body={"osName":"buyMedicine","appKey":"231282000001","methodName":"queryTaskList2998","channel":"jdapp","imei":"OOLPCJFL","location":{"district":"1234","town":"45678","province":"16","city":"1234"},"wrapResult":true,"appId":"2d0050d8877d4065b6227fb9fdcc01bb","version":12}&build=169227&client=apple&clientVersion=12.6.2&functionId=jdh_laputa_handleSoaRequest_reinforce`,
-                cookie
+                cookie,
+                headers
             }
         )
         for (let i of this.haskey(list, 'data.result')) {
@@ -72,18 +79,20 @@ class Main extends Template {
                         // console.log(j)
                         if (j.status == 1) {
                             let d = await this.sign.jdCurl({
-                                    'url': `https://api.m.jd.com/client.action?functionId=jdh_laputa_handleSoaRequest_reinforce`,
-                                    'form': `body={"osName":"buyMedicine","infoId":"jdhHome_task","methodName":"doTask2745","encodeId":"${j.encodeId}","appKey":"231282000001","channel":"jdapp","wrapResult":true,"appId":"2d0050d8877d4065b6227fb9fdcc01bb","version":12,"imei":"OOLPCJFL","location":{"district":"1234","town":"45678","province":"16","city":"1234"}}&build=169227&client=apple&clientVersion=12.6.2&functionId=jdh_laputa_handleSoaRequest_reinforce`,
-                                    cookie
+                                    'url': `https://api.m.jd.com/client.action`,
+                                    'form': `body={"osName":"buyMedicine","infoId":"jdhHome_task","methodName":"doTask2745","encodeId":"${j.encodeId}","appKey":"231282000001","channel":"jdapp","wrapResult":true,"appId":"2d0050d8877d4065b6227fb9fdcc01bb","version":12,"imei":"OOLPCJFL","location":{"district":"1234","town":"45678","province":"16","city":"1234"}}&build=169227&client=apple&clientVersion=12.6.2&functionId=jdh_msoa_doTaskGw`,
+                                    cookie,
+                                    headers
                                 }
                             )
-                            console.log(this.haskey(d, 'data.result') || d)
+                            console.log(this.haskey(d, 'result.result') || d)
                             await this.wait(5000)
                         }
                         let r = await this.sign.jdCurl({
-                                'url': `https://api.m.jd.com/client.action?functionId=jdh_laputa_handleSoaRequest_reinforce`,
-                                'form': `body={"osName":"buyMedicine","version":12,"wrapResult":true,"methodName":"sendAward2999","queryToken":"${j.queryToken}","appId":"2d0050d8877d4065b6227fb9fdcc01bb","infoId":"jdhHome_task","activityId":8542,"channel":"jdapp","location":{"district":"1234","town":"45678","province":"16","city":"1234"},"imei":"OOLPCJFL","taskId":${j.id},"appKey":"231282000001"}&build=169227&client=apple&clientVersion=12.6.2&functionId=jdh_laputa_handleSoaRequest_reinforce`,
-                                cookie
+                                'url': `https://api.m.jd.com/client.action`,
+                                'form': `body={"osName":"buyMedicine","version":12,"wrapResult":true,"methodName":"sendAward2999","queryToken":"${j.queryToken}","appId":"2d0050d8877d4065b6227fb9fdcc01bb","infoId":"jdhHome_task","activityId":8542,"channel":"jdapp","location":{"district":"1234","town":"45678","province":"16","city":"1234"},"imei":"OOLPCJFL","taskId":${j.id},"appKey":"231282000001"}&build=169227&client=apple&clientVersion=12.6.2&functionId=jdh_msoa_sendAwardGw`,
+                                cookie,
+                                headers
                             }
                         )
                         gift.call(this, r)
