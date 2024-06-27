fd3a0fed327c0523c09caca65845125bda62c5ae
可达鸭 <ii@iippcc.com>
2024-06-27 17:07:09 +0800

    Update jd_task_healthy.js

--- parse/jd/jd_task_healthy.js
+++ parse/jd/jd_task_healthy.js
@@ -8,6 +8,7 @@ class Main extends Template {
         this.task = 'local'
         this.import = ['jdSign']
         this.readme = `此活动需要验证sign,需要自己docker部署sign算法\n只做京豆任务`
+        this.interval = 6000
     }
 
     async prepare() {
@@ -28,28 +29,35 @@ class Main extends Template {
                 }
             }
         }
-        let headers = {
-            referer: 'https://jd.com',
-            'j-e-c': `%7B%22ciphertype%22:5,%22cipher%22:%7B%22pin%22:%22awHpZXZ1JQrEHW9uHvvB%22%7D,%22ts%22:${parseInt(new Date().getTime()/1000)},%22hdid%22:%22JM9F1ywUPwflvMIpYPok0tt5k9kW4ArJEU3lfLhxBqw=%22,%22version%22:%221.0.3%22,%22appname%22:%22com.360buy.jdmobile%22,%22ridx%22:-1%7D`,
-            'jdgs': `{"b1":"${this.uuid(8)}-${this.uuid(4)}-${this.uuid(4)}-${this.uuid(4)}-${this.uuid(12)}","b2":"2.2.4_1","b3":"2.1","b4":"","b5":"${this.sha1(p.cookie)}","b7": ${new Date().getTime().toString()},"b6":"${this.sha1(p.user)}"}`
-        }
-        let h = await this.sign.jdCurl({
-                'url': `https://api.m.jd.com/client.action`,
-                'form': `body={"osName":"buyMedicine","channel":"jdapp","methodName":"getKitTask2727","groupCode":"openkits","m_patch_appKey":"231282000001","imei":"OOLPCJFL","wrapResult":true,"appId":"2d0050d8877d4065b6227fb9fdcc01bb","version":12,"location":{"district":"1234","town":"56789","province":"16","city":"1234"}}&build=169227&client=apple&clientVersion=12.6.2&functionId=jdh_laputa_handleSoaRequest_reinforce`,
-                cookie,
-                headers
+        for (let i of Array(3)) {
+            var headers = {
+                referer: 'https://jd.com',
+                'j-e-c': `%7B%22ciphertype%22:5,%22cipher%22:%7B%22pin%22:%22awHpZXZ1JQrEHW9uHvvB%22%7D,%22ts%22:${parseInt(new Date().getTime() / 1000)},%22hdid%22:%22JM9F1ywUPwflvMIpYPok0tt5k9kW4ArJEU3lfLhxBqw=%22,%22version%22:%221.0.3%22,%22appname%22:%22com.360buy.jdmobile%22,%22ridx%22:-1%7D`,
+                jdgs: `{"b1":"${this.uuid(8)}-${this.uuid(4)}-${this.uuid(4)}-${this.uuid(4)}-${this.uuid(12)}","b2":"2.1.6_1","b3":"2.0","b4":"","b5":"${this.sha1(p.cookie)}","b7":"${new Date().getTime().toString()}","b6":"${this.sha1(p.user)}"}"}`
             }
-        )
+            var h = await this.sign.jdCurl({
+                    'url': `https://api.m.jd.com/client.action`,
+                    'form': `body={"osName":"buyMedicine","channel":"jdapp","methodName":"getKitTask2727","groupCode":"openkits","m_patch_appKey":"231282000001","imei":"OOLPCJFL","wrapResult":true,"appId":"2d0050d8877d4065b6227fb9fdcc01bb","version":12,"location":{"district":"1234","town":"56789","province":"16","city":"1234"}}&build=169227&client=apple&clientVersion=12.6.2&functionId=jdh_laputa_handleSoaRequest_reinforce`,
+                    cookie,
+                    headers
+                }
+            )
+            if (this.haskey(h, 'data.result.encodeId')) {
+                break
+            }
+            await this.wait(1000)
+        }
         let encodeId = this.haskey(h, 'data.result.encodeId')
         if (!encodeId) {
             console.log("没有获取到数据...")
             return
         }
         let res = h.data.result
-        if (res.status == 1) {
+        // console.log(res.status)
+        if (res.status) {
             let sign = await this.sign.jdCurl({
                     'url': `https://api.m.jd.com/client.action`,
-                    'form': `body={"osName":"buyMedicine","infoId":"jdhHome_task","methodName":"doTask2745","encodeId":"${encodeId}","appKey":"231282000001","channel":"jdapp","wrapResult":true,"appId":"2d0050d8877d4065b6227fb9fdcc01bb","version":12,"imei":"OOLPCJFL","location":{"district":"1244","town":"56789","province":"16","city":"1234"}}&build=169227&client=apple&clientVersion=12.6.2&functionId=jdh_msoa_doTaskGw`,
+                    'form': `body={"infoId":"jdhHome_task","appKey":"231282000001","channel":"jdapp","encodeId":"${encodeId}","imei":"OKFCDBBG","location":{"district":"55674","town":"","province":"1","city":"72"},"wrapResult":true}&build=169227&client=apple&clientVersion=12.6.2&functionId=jdh_msoa_doTaskGw`,
                     cookie, headers
                 }
             )
