43ff6408464eb5852151b5abd18aaa39e4c2e4a4
可达鸭 <ii@iippcc.com>
2024-08-07 21:56:35 +0800

    支持使用优惠券保价

--- parse/jd/jd_task_price.js
+++ parse/jd/jd_task_price.js
@@ -28,10 +28,26 @@ class Main extends Template {
             }
         )
         if (this.haskey(s, 'data.succAmount')) {
-            console.log(`保价: ${s.data.succAmount}`, p.user)
+            this.print(`保价: ${s.data.succAmount}`, p.user)
+        }
+        else if (this.haskey(s, 'data.confirmCouponInfos.0.couponId')) {
+            let onceBatchId = this.haskey(s, 'data.onceBatchId')
+            console.log("当前可用优惠券保价")
+            let ss = await this.curl({
+                    'url': `https://api.m.jd.com/`,
+                    'form': `functionId=mlproprice_skuOnceApply_jsf&appid=price_protection&loginType=2&body={"onceBatchId":"${onceBatchId}","couponConfirmFlag":1,"type":"25"}&client=apple&clientVersion=&x-api-eid-token=jdd03UT42BFT33TGS6GOIOWXCCOFR2T5UM44HG27BZ3JBLL5TQWMEHHCGMANY7T3YNDDBPISS4SS7Z7C7T3OFBOP5QFT2KIAAAAMRENKUBUQAAAAACBZLQEQUA7ANXQX&h5st=&t=1722874735852`,
+                    cookie
+                }
+            )
+            if (this.haskey(ss, 'data.succAmount')) {
+                this.print(`用券保价: ${ss.data.succAmount}`, p.user)
+            }
+            else {
+                console.log(this.haskey(ss, ['data.onceApplyNoSuccessTips', 'data.responseMessage']) || s)
+            }
         }
         else {
-            console.log(this.haskey(s, 'data.onceApplyNoSuccessTips') || s)
+            console.log(this.haskey(s, ['data.onceApplyNoSuccessTips', 'data.responseMessage']) || s)
         }
     }
 }
