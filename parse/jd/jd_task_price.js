5cedf428ecc70120dc1e859c6873aa9cad15abbc
可达鸭 <ii@iippcc.com>
2024-09-20 09:46:58 +0800

    Update jd_task_price.js

--- parse/jd/jd_task_price.js
+++ parse/jd/jd_task_price.js
@@ -8,6 +8,9 @@ class Main extends Template {
         this.import = ['jdAlgo']
         this.task = 'local'
         this.interval = 5000
+        this.hint = {
+            coupon: '1,默认不使用券保价'
+        }
     }
 
     async prepare() {
@@ -32,18 +35,23 @@ class Main extends Template {
         }
         else if (this.haskey(s, 'data.confirmCouponInfos.0.couponId')) {
             let onceBatchId = this.haskey(s, 'data.onceBatchId')
-            console.log("当前可用优惠券保价")
-            let ss = await this.curl({
-                    'url': `https://api.m.jd.com/`,
-                    'form': `functionId=mlproprice_skuOnceApply_jsf&appid=price_protection&loginType=2&body={"onceBatchId":"${onceBatchId}","couponConfirmFlag":1,"type":"25"}&client=apple&clientVersion=&x-api-eid-token=jdd03UT42BFT33TGS6GOIOWXCCOFR2T5UM44HG27BZ3JBLL5TQWMEHHCGMANY7T3YNDDBPISS4SS7Z7C7T3OFBOP5QFT2KIAAAAMRENKUBUQAAAAACBZLQEQUA7ANXQX&h5st=&t=1722874735852`,
-                    cookie
+            if (this.profile.coupon) {
+                console.log("当前可用优惠券保价")
+                let ss = await this.curl({
+                        'url': `https://api.m.jd.com/`,
+                        'form': `functionId=mlproprice_skuOnceApply_jsf&appid=price_protection&loginType=2&body={"onceBatchId":"${onceBatchId}","couponConfirmFlag":1,"type":"25"}&client=apple&clientVersion=&x-api-eid-token=jdd03UT42BFT33TGS6GOIOWXCCOFR2T5UM44HG27BZ3JBLL5TQWMEHHCGMANY7T3YNDDBPISS4SS7Z7C7T3OFBOP5QFT2KIAAAAMRENKUBUQAAAAACBZLQEQUA7ANXQX&h5st=&t=1722874735852`,
+                        cookie
+                    }
+                )
+                if (this.haskey(ss, 'data.succAmount')) {
+                    this.print(`用券保价: ${ss.data.succAmount}`, p.user)
                 }
-            )
-            if (this.haskey(ss, 'data.succAmount')) {
-                this.print(`用券保价: ${ss.data.succAmount}`, p.user)
+                else {
+                    console.log(this.haskey(ss, ['data.onceApplyNoSuccessTips', 'data.responseMessage']) || s)
+                }
             }
             else {
-                console.log(this.haskey(ss, ['data.onceApplyNoSuccessTips', 'data.responseMessage']) || s)
+                this.print("有可用券可以保价", p.user)
             }
         }
         else {
