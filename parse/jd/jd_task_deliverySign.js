d9c3077de97cfcf317121c81ae63a17d4bb5ca54
可达鸭 <ii@iippcc.com>
2024-10-24 01:12:36 +0800

    Create jd_task_deliverySign.js

---
+++ parse/jd/jd_task_deliverySign.js
@@ -0,0 +1,61 @@
+const Template = require('../../template');
+
+class Main extends Template {
+    constructor() {
+        super()
+        this.title = "京东天天领豆"
+        this.cron = `${this.rand(0, 59)} ${this.rand(6, 22)} * * *`
+        this.task = 'local'
+        this.import = ['jdAlgo']
+        this.interval = 2000
+    }
+
+    async prepare() {
+        this.algo = new this.modules.jdAlgo({
+            type: "main",
+            version: "latest"
+        })
+    }
+
+    async main(p) {
+        let cookie = p.cookie;
+        let home = await this.algo.curl({
+                'url': `https://api.m.jd.com/`,
+                'form': `functionId=deliverySign_home&appid=signed_wh5_ihub&body={"activityId":"2925"}&client=apple&clientVersion=13.2.8&d_model=&osVersion=15.1.1`,
+                cookie,
+                algo: {
+                    appId: 'e88fd'
+                }
+            }
+        )
+        if (this.haskey(home, 'data.result.bubbleList')) {
+            for (let i of home.data.result.bubbleList) {
+                if (i.status == 1) {
+                    let sign = await this.algo.curl({
+                            'url': `https://api.m.jd.com/`,
+                            'form': `functionId=deliverySign_sign&appid=signed_wh5_ihub&body={"activityId":"2925"}&client=apple&clientVersion=13.2.8&d_model=&osVersion=15.1.1`,
+                            cookie,
+                            algo: {
+                                appId: 'e88fd'
+                            }
+                        }
+                    )
+                    if (this.haskey(sign, 'data.result.value')) {
+                        this.print(`京豆: ${sign.data.result.value}`, p.user)
+                    }
+                    else {
+                        console.log(sign)
+                    }
+                }
+                else {
+                    console.log("暂不可做:", i.text)
+                }
+            }
+        }
+        else {
+            console.log("没有获取到数据...")
+        }
+    }
+}
+
+module.exports = Main;
