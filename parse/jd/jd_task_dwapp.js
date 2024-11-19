d222d376073f6cd2b3a490fb320ce7f92592cacd
可达鸭 <ii@iippcc.com>
2024-11-19 19:54:18 +0800

    Revert "Update jd_task_dwapp.js"
    This reverts commit ca64f113440362676641aa3ccc60badc0cb68096.

--- parse/jd/jd_task_dwapp.js
+++ parse/jd/jd_task_dwapp.js
@@ -70,8 +70,7 @@ class Main extends Template {
                 algo: {
                     appId: '60d0e'
                 },
-                referer: 'https://prodev.m.jd.com/mall/active/eEcYM32eezJB7YX4SBihziJCiGV/index.html',
-                ciphers: "TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256"
+                referer: 'https://mypoint.jd.com/'
             }
         )
         console.log(sign)
