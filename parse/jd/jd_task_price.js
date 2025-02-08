40ac33bf06751b427cee5297d8e3649674a8324c
可达鸭 <ii@iippcc.com>
2025-02-08 21:46:06 +0800

    Update jd_task_price.js

--- parse/jd/jd_task_price.js
+++ parse/jd/jd_task_price.js
@@ -15,19 +15,19 @@ class Main extends Template {
 
     async prepare() {
         this.algo = new this.modules.jdAlgo()
-        this.algo.set({
-            'appId': 'd2f64',
-            'type': 'web',
-            "version": "3.1",
-        })
     }
 
     async main(p) {
         let cookie = p.cookie
-        let s = await this.curl({
+        let s = await this.algo.curl({
                 'url': `https://api.m.jd.com/`,
-                'form': `functionId=mlproprice_skuOnceApply_jsf&appid=price_protection&loginType=2&body={"onceBatchId":"","couponConfirmFlag":null,"type":"25"}&client=apple&clientVersion=&x-api-eid-token=jdd03C3HUEKC6G2V5WV6SOXJV5E4J2ILKIIHLPARTU7DKUSMS72ICFUVMMF7ZVZXDON6VLTUCVU2GNZ2RZRMVIDXGF2FBMUAAAAMQFQIBMFAAAAAACIQ46Z6H2VWO6MX&h5st=&t=1718726274981`,
-                cookie
+                'form': `functionId=mlproprice_skuOnceApply_jsf&appid=price_protection&loginType=2&body={"onceBatchId":"","couponConfirmFlag":null,"type":"25"}&client=apple&clientVersion=15.8.1&x-api-eid-token=jdd03C3HUEKC6G2V5WV6SOXJV5E4J2ILKIIHLPARTU7DKUSMS72ICFUVMMF7ZVZXDON6VLTUCVU2GNZ2RZRMVIDXGF2FBMUAAAAMQFQIBMFAAAAAACIQ46Z6H2VWO6MX&h5st=&t=1718726274981`,
+                cookie,
+                algo: {
+                    status: true,
+                    appId: '6f46e'
+                },
+                shell: 1
             }
         )
         if (this.haskey(s, 'data.succAmount')) {
@@ -37,10 +37,14 @@ class Main extends Template {
             let onceBatchId = this.haskey(s, 'data.onceBatchId')
             if (this.profile.coupon) {
                 console.log("当前可用优惠券保价")
-                let ss = await this.curl({
+                let ss = await this.algo.curl({
                         'url': `https://api.m.jd.com/`,
-                        'form': `functionId=mlproprice_skuOnceApply_jsf&appid=price_protection&loginType=2&body={"onceBatchId":"${onceBatchId}","couponConfirmFlag":1,"type":"25"}&client=apple&clientVersion=&x-api-eid-token=jdd03UT42BFT33TGS6GOIOWXCCOFR2T5UM44HG27BZ3JBLL5TQWMEHHCGMANY7T3YNDDBPISS4SS7Z7C7T3OFBOP5QFT2KIAAAAMRENKUBUQAAAAACBZLQEQUA7ANXQX&h5st=&t=1722874735852`,
-                        cookie
+                        'form': `functionId=mlproprice_skuOnceApply_jsf&appid=price_protection&loginType=2&body={"onceBatchId":"${onceBatchId}","couponConfirmFlag":1,"type":"25"}&client=apple&clientVersion=15.8.1&x-api-eid-token=jdd03UT42BFT33TGS6GOIOWXCCOFR2T5UM44HG27BZ3JBLL5TQWMEHHCGMANY7T3YNDDBPISS4SS7Z7C7T3OFBOP5QFT2KIAAAAMRENKUBUQAAAAACBZLQEQUA7ANXQX&h5st=&t=1722874735852`,
+                        cookie,
+                        shell: 1,
+                        algo: {
+                            appId: '6f46e'
+                        }
                     }
                 )
                 if (this.haskey(ss, 'data.succAmount')) {
